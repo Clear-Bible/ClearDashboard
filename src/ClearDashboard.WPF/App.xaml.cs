c1ff2661c8aaca39114047ff3a8899b0195e5922
GeoDirk <geodirk@gmail.com>
2021-12-28 10:40:11 -0500

    Initial Commit with MVVMCross 8 setup

---
+++ src/ClearDashboard.WPF/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+using Microsoft.Extensions.Logging;
+using MvvmCross.Core;
+using MvvmCross.Platforms.Wpf.Core;
+using MvvmCross.Platforms.Wpf.Views;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace ClearDashboard.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : MvxApplication
+    {
+        public App()
+        {
+            this.RegisterSetupType<Setup>();
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            // Process unhandled exception
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            // Prevent default unhandled exception processing
+            e.Handled = true;
+        }
+
+    }
+
+}
---
+++ src/ClearDashboard/App.cs
@@ -0,0 +1,18 @@
+﻿using ClearDashboard.Core.ViewModels;
+using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+
+namespace ClearDashboard.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+            RegisterAppStart<StartupViewModel>();
+        }
+    }
+}
