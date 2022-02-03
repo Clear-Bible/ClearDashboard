c4ce475f6b09a43f05e9282b748f93a655b6391c
Elliott Stoddard <eds@k7try.com>
2022-02-03 09:31:40 -0500

    Initial template design.

---
+++ src/ClearDashboard.Wpf/Template.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClearDashboard.Common;
+using ClearDashboard.DAL;
+
+namespace ClearDashboard.Wpf
+{
+    internal class Template
+    {
+        #region Member Variables
+
+        #endregion //Member Variables
+
+        #region Public Properties
+
+        #endregion //Public Properties
+
+        #region Observable Properties
+
+        #endregion //Observable Properties
+
+        #region Constructor
+
+        #endregion //Constructor
+
+        #region Methods
+
+        #endregion // Methods
+
+    }
+}
