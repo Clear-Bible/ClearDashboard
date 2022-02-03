71e7174faa4638411386b60ccf0f3cd5daf589e4
Elliott Stoddard <24723108+K7TRY@users.noreply.github.com>
2022-02-03 12:48:59 -0500

    Dal setup (#8)
    * Merge with main.
    
    * Initial code setup for the DAL.

---
+++ src/ClearDashboard.DAL/Models/ENums.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClearDashboard.DAL.Models
+{
+    public static class ENums
+    {
+        /// <summary>
+        /// Used to keep track of what type of object a NoteTagModel is. 
+        /// </summary>
+        public enum NoteTagType
+        {
+            [Description("Note")]
+            [Display(Name = "Note")]
+            Note = 1,
+
+            [Description("Note")]
+            [Display(Name = "Tag")]
+            Tag = 2
+        }
+    }
+}
