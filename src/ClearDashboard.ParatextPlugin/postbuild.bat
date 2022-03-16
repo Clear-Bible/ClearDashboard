786fc235884d0e54546826840a362ee2f00d5ff7
Michael Gerfen <gerfen@hotmail.com>
2022-03-15 23:23:58 -0700

    Got plugin published to Paratext plugin directory.

---
+++ src/ClearDashboard.ParatextPlugin/postbuild.bat
@@ -0,0 +1,21 @@
+﻿
+set ProjectName="ClearDashboardPlugin"
+set TargetPath=.\bin\Debug\net48
+set NamedPipesPath=.\..\NamedPipes\bin\Debug\net48
+
+if exist "%ParatextInstallDir%\plugins\%ProjectName%"\ (
+  del /F /Q "%ParatextInstallDir%\plugins\%ProjectName%"\*.*
+) else (
+  mkdir "%ParatextInstallDir%\plugins\%ProjectName%"
+)
+
+@echo Copying files to %ParatextInstallDir%\plugins\%ProjectName%
+xcopy "%TargetPath%\*.dll" "%ParatextInstallDir%\plugins\%ProjectName%" /y /i
+xcopy "%TargetPath%\*.pdb" "%ParatextInstallDir%\plugins\%ProjectName%" /y /i
+xcopy "%TargetPath%\Plugin.bmp" "%ParatextInstallDir%\plugins\%ProjectName%" /y /i
+
+rename "%ParatextInstallDir%\plugins\%ProjectName%\ClearDashboard.ParatextPlugin.dll" "ClearDashboard.ParatextPlugin.ptxplg"
+
+
+@echo Copying Named Pipes dll files to %ParatextInstallDir%\plugins\%ProjectName%
+xcopy "%NamedPipesPath%\*.dll" "%ParatextInstallDir%\plugins\%ProjectName%" /y /i
