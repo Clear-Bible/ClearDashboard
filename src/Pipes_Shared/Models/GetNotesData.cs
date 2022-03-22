34102b9f8e7bf7eabace59cc17ba3ff07336d674
GeoDirk <geodirk@gmail.com>
2022-03-22 10:34:18 -0400

    Removed the NamedPipes project

---
+++ src/Pipes_Shared/Models/GetNotesData.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using MvvmHelpers;
+
+namespace ClearDashboard.NamedPipes.Models
+{
+    public class GetNotesData : ObservableObject
+    {
+        private int _bookID;
+        [JsonProperty]
+        public int BookID
+        {
+            get => _bookID;
+            set
+            {
+                _bookID = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _chapterID;
+        [JsonProperty]
+        public int ChapterID
+        {
+            get => _chapterID;
+            set
+            {
+                _chapterID = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _includeResolved;
+        [JsonProperty]
+        public bool IncludeResolved
+        {
+            get => _includeResolved;
+            set
+            {
+                _includeResolved = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
