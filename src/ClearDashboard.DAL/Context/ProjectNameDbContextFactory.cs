edbb71d920a3292cfcacb91b666b26ffd5aa4098
Michael Gerfen <gerfen@hotmail.com>
2022-03-29 18:47:21 -0700

    Returning ProjectAssets instead of AlignmentContext

--- src/ClearDashboard.DAL/Context/ProjectNameDbContextFactory.cs
+++ src/ClearDashboard.DAL/Context/ProjectNameDbContextFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using ClearDashboard.DataAccessLayer.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -9,7 +10,7 @@ namespace ClearDashboard.DataAccessLayer.Context
 {
     public interface IProjectNameDbContextFactory<TDbContext> where TDbContext : DbContext
     {
-        Task<TDbContext> Create(string connectionString);
+        Task<ProjectAssets> Create(string connectionString);
     }
 
     public class ProjectNameDbContextFactory : IProjectNameDbContextFactory<AlignmentContext>
@@ -23,36 +24,50 @@ namespace ClearDashboard.DataAccessLayer.Context
             _logger = logger;
         }
 
-        public async Task<AlignmentContext> Create(string projectName)
+        public async Task<ProjectAssets> Create(string projectName)
         {
-            var directoryPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.Personal)}\\ClearDashboard_Projects\\{projectName}";
+            var projectAssets = new ProjectAssets
+            {
+                ProjectName = projectName,
+                ProjectDirectory = EnsureProjectDirectory(projectName),
+            };
+
+            projectAssets.AlignmentContext = await GetAlignmentContext(projectAssets.DataContextPath);
+            return projectAssets;
+
+        }
+
+        private async Task<AlignmentContext> GetAlignmentContext(string fullPath)
+        {
+            var context = _serviceProvider.GetService<AlignmentContext>();
+            if (context != null)
+            {
+                try
+                {
+                    _logger.LogInformation($"Attempting to create or migrate '{fullPath}'");
+                    context.DatabasePath = fullPath;
+                    await context.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "An error occurred while creating an instance the AlignmentContext.");
+                    throw;
+                }
+                return context;
+            }
+            throw new NullReferenceException("Please ensure 'AlignmentContext' has been registered with the dependency injection container.");
+        }
+
+        private string EnsureProjectDirectory(string projectName)
+        {
+            var directoryPath =  $"{Environment.GetFolderPath(Environment.SpecialFolder.Personal)}\\ClearDashboard_Projects\\{projectName}";
             if (!Directory.Exists(directoryPath))
             {
                 _logger.LogInformation($"Creating project directory {directoryPath}.");
                 Directory.CreateDirectory(directoryPath);
             }
-            var fullPath = $"{directoryPath}\\{projectName}.sqlite";
 
-           var context = _serviceProvider.GetService<AlignmentContext>();
-
-           if (context != null)
-           {
-               try
-               {
-                   _logger.LogInformation($"Attempting to create or migrate '{fullPath}'");
-                   context.DatabasePath = fullPath;
-                   await context.Migrate();
-               }
-               catch (Exception ex)
-               {
-                    _logger.LogError(ex, "An error occurred while creating an instance the AlignmentContext.");
-                   throw;
-               }
-
-               return context;
-           }
-
-           throw new NullReferenceException("Please ensure 'AlignmentContext' has been registered with the dependency injection container.");
+            return directoryPath;
         }
     }
 }
