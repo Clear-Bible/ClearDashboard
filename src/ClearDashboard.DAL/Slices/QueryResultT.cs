68d786a2af7ecd5cdbe1d19288522da691994fbd
Michael Gerfen <gerfen@hotmail.com>
2022-04-16 16:16:56 -0700

    Added a base class which can be used to execute commands against databases found in the executable's "resource" directory.

---
+++ src/ClearDashboard.DAL/Slices/QueryResultT.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Threading.Tasks;
+
+namespace ClearDashboard.DataAccessLayer.Slices
+{
+    public abstract class Result<T>
+    {
+        protected Result(T? result, bool success = true, string message = "Success")
+        {
+            Success = success;
+            Message = message;
+            Data = result;
+        }
+
+        public T? Data { get; set; }   
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class QueryResult<T> : Result<T>
+    {
+        public QueryResult(T? result, bool success = true, string message = "Success") : base(result, success, message)
+        {
+        }
+    }
+
+    public class CommandResult<T> : Result<T>
+    {
+        public CommandResult(T? result, bool success = true, string message = "Success") : base(result, success, message)
+        {
+        }
+    }
+}
