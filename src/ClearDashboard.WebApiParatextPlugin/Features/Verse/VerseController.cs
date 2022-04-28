93b48468f9903768f770b3d989a95f32d3a1354f
Michael Gerfen <gerfen@hotmail.com>
2022-04-28 11:04:40 -0700

    Added support for getting the current verse from Paratext

---
+++ src/ClearDashboard.WebApiParatextPlugin/Features/Verse/VerseController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using ParaTextPlugin.Data.Features;
+using ParaTextPlugin.Data.Features.Verse;
+
+namespace ClearDashboard.WebApiParatextPlugin.Features.Verse
+{
+    public class VerseController : FeatureSliceController
+    {
+        public VerseController(IMediator mediator, ILogger<VerseController> logger) : base(mediator, logger)
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<QueryResult<string>> GetAsync([FromBody] GetCurrentVerseCommand command)
+        {
+            return await ExecuteCommandAsync<QueryResult<string>, string>(command, CancellationToken.None);
+
+        }
+
+    }
+}
--- src/ClearDashboard.WebApiParatextPlugin/Features/Verse/VerseSlice.cs
+++ src/ClearDashboard.WebApiParatextPlugin/Features/Verse/VerseSlice.cs
@@ -1,8 +1,47 @@
 ﻿
 
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ClearDashboard.WebApiParatextPlugin.Features.Project;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Paratext.PluginInterfaces;
+using ParaTextPlugin.Data.Features;
+using ParaTextPlugin.Data.Features.Project;
+using ParaTextPlugin.Data.Features.Verse;
+
 namespace ClearDashboard.WebApiParatextPlugin.Features.Verse
 {
-    public class VerseSlice
+    public class GetCurrentVerseCommandHandler : IRequestHandler<GetCurrentVerseCommand, QueryResult<string>>
     {
+        private readonly IVerseRef _verseRef;
+        private readonly ILogger<GetCurrentVerseCommandHandler> _logger;
+
+        public GetCurrentVerseCommandHandler(IVerseRef verseRef, ILogger<GetCurrentVerseCommandHandler> logger)
+        {
+            _verseRef = verseRef;
+            _logger = logger;
+        }
+        public Task<QueryResult<string>> Handle(GetCurrentVerseCommand request, CancellationToken cancellationToken)
+        {
+            var queryResult = new QueryResult<string>(string.Empty);
+            try
+            {
+                var verseId = _verseRef.BBBCCCVVV.ToString();
+                if (verseId.Length < 8)
+                {
+                    verseId = verseId.PadLeft(8, '0');
+                }
+                queryResult.Data = verseId;
+               
+            }
+            catch (Exception ex)
+            {
+                queryResult.Success = false;
+                queryResult.Message = ex.Message;
+            }
+            return Task.FromResult(queryResult);
+        }
     }
 }
