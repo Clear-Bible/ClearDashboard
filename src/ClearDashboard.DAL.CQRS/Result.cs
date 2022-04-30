e1742b7da6e5f620677ac43446e3ed428ab81159
Michael Gerfen <gerfen@hotmail.com>
2022-04-30 16:55:49 -0700

    Added an assembly to share QueryResult<T> between the DAL and the Paratext plugin

---
+++ src/ClearDashboard.DAL.CQRS/Result.cs
@@ -0,0 +1,15 @@
+﻿namespace ClearDashboard.DAL.CQRS;
+
+public abstract class Result<T>
+{
+    protected Result(T? result, bool success = true, string message = "Success")
+    {
+        Success = success;
+        Message = message;
+        Data = result;
+    }
+
+    public T? Data { get; set; }   
+    public bool Success { get; set; }
+    public string Message { get; set; }
+}
