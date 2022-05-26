31ee656535928e032358d73f69d7f51895b61752
GeoDirk <geodirk@gmail.com>
2022-05-26 10:21:13 -0400

    Updated Bible Books to include the full Paratext list - oh my....

---
+++ src/ClearDashboard.DAL.Models/Common/PinsDataTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearDashboard.DataAccessLayer.Models.Common
+{
+    public class PinsDataTable
+    {
+        // 0. Source, 1. Lform, 2. Gloss, 3. Lang, 4. Refs, 5. Code, 6. Match, 7. Notes, 8. SimpRefs, 
+        // 9. Phrase, 10. Word, 11. Prefix, 12. Stem, 13. Suffix
+        public string Source { get; set; }
+
+        public string Lform { get; set; }
+
+        public string Gloss { get; set; }
+
+        public string Lang { get; set; }
+
+        public string Refs { get; set; }
+
+        public string Code { get; set; }
+
+        public string Match { get; set; }
+
+        public string Notes { get; set; }
+
+        public string SimpRefs { get; set; }
+
+        public string Phrase { get; set; }
+
+        public string Word { get; set; }
+
+        public string Prefix { get; set; }
+
+        public string Stem { get; set; }
+
+        public string Suffix { get; set; }
+    }
+}
--- src/ClearDashboard.WPF/ViewModels/PinsViewModel.cs
+++ src/ClearDashboard.WPF/ViewModels/PinsViewModel.cs
@@ -10,7 +10,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
+using ClearDashboard.DataAccessLayer.Models.Common;
 using ClearDashboard.DataAccessLayer.Paratext;
+using SIL.ObjectModel;
 
 namespace ClearDashboard.Wpf.ViewModels
 {
@@ -22,6 +24,8 @@ namespace ClearDashboard.Wpf.ViewModels
         private TermRenderingsList _termRenderingsList = new ();
         private BiblicalTermsList _biblicalTermsList = new ();
         private BiblicalTermsList _allBiblicalTermsList = new ();
+        private SpellingStatus _spellingStatus = new ();
+        private ObservableList<PinsDataTable> _thedata = new ();
 
         #endregion //Member Variables
 
@@ -66,7 +70,7 @@ namespace ClearDashboard.Wpf.ViewModels
                 string xmlPath = Path.Combine(ProjectManager.CurrentDashboardProject.DirectoryPath,
                     "TermRenderings.xml");
 
-                if (!File.Exists(xmlPath))
+                if (File.Exists(xmlPath))
                 {
                     XmlDocument doc = new XmlDocument();
                     doc.Load(xmlPath);
@@ -167,6 +171,194 @@ namespace ClearDashboard.Wpf.ViewModels
             }
 
 
+            // fix the greek renderings which are inconsistent
+            for (int i = _termRenderingsList.TermRendering.Count - 1; i >= 0; i--)
+            {
+                if (_termRenderingsList.TermRendering[i].Renderings == "")
+                {
+                    // remove any records without rendering data
+                    _termRenderingsList.TermRendering.RemoveAt(i);
+                }
+                else
+                {
+                    _termRenderingsList.TermRendering[i].Id =
+                        CorrectUnicode(_termRenderingsList.TermRendering[i].Id);
+                }
+            }
+
+            for (int i = _biblicalTermsList.Term.Count - 1; i >= 0; i--)
+            {
+                if (_biblicalTermsList.Term[i].Id == "")
+                {
+                    _biblicalTermsList.Term[i].Id =
+                        CorrectUnicode(_biblicalTermsList.Term[i].Id);
+                }
+            }
+
+            for (int i = _allBiblicalTermsList.Term.Count - 1; i >= 0; i--)
+            {
+                if (_allBiblicalTermsList.Term[i].Id == "")
+                {
+                    _allBiblicalTermsList.Term[i].Id =
+                        CorrectUnicode(_allBiblicalTermsList.Term[i].Id);
+                }
+            }
+
+
+            // load in the spellingstatus.xml
+            await Task.Run(() =>
+            {
+                string xmlPath = Path.Combine(ProjectManager.CurrentDashboardProject.DirectoryPath,
+                    "SpellingStatus.xml");
+
+                if (File.Exists(xmlPath))
+                {
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(xmlPath);
+                    XmlNodeReader reader = new XmlNodeReader(doc);
+
+                    using (reader)
+                    {
+                        XmlSerializer serializer = new XmlSerializer(typeof(SpellingStatus));
+                        try
+                        {
+                            _spellingStatus = (SpellingStatus)serializer.Deserialize(reader);
+                        }
+                        catch (Exception e)
+                        {
+                            Logger.LogError("Error in PINS deserialization of AllBibilicalTerms.xml: " + e.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    Logger.LogError("Missing file in PINS viewmodel: " + xmlPath);
+                }
+            }).ConfigureAwait(false);
+
+            Console.WriteLine();
+
+            foreach (var terms in _termRenderingsList.TermRendering)
+            {
+                string target = terms.Renderings;
+                target = target.Replace("||", "; ");
+
+                if (target == "Ijip")
+                {
+                    Console.WriteLine();
+                }
+
+
+                string source = terms.Id;
+
+                string pbtsense;
+                string pbtspell;
+                if (source.Contains("."))
+                {                                                                                                               // Sense number uses "." in gateway language; this Sense number will not match anything in abt or bbt
+                    pbtsense = source[..source.IndexOf(".")] + " {" + source[(source.IndexOf(".") + 1)..] + "}";    // place Sense in braces  
+                    pbtspell = source = source[..source.IndexOf(".")];                                                 // remove the Sense number from word/phrase for correct matching with AllBiblicalTerms
+                }
+                else if (source.Contains("-"))                                                                               // Sense number uses "-" in Gk & Heb, this will match bbt
+                {
+                    pbtsense = source.Trim().Replace("-", " {") + "}";                                                       // place Sense in braces
+                    pbtspell = source[..source.IndexOf("-")];                                                             // remove the Sense number from word/phrase for correct matching with Spelling
+                }
+                else
+                    pbtspell = pbtsense = source;
+
+
+                // CHECK AGAINST SPELLING
+                var spellingRecords = _spellingStatus.Status.FindAll(s => s.Word.ToLower() == pbtspell.ToLower());
+                if (spellingRecords.Count == 0)
+                {
+                    pbtspell = "";
+                }
+                else
+                {
+                    if (spellingRecords[0].State == "W")
+                    {
+                        // misspelled
+                        pbtspell = " [misspelled]";
+                    } 
+                    else if (spellingRecords[0].SpecificCase != pbtspell)
+                    {
+                        //   has wrong case
+                        pbtspell = " [" + spellingRecords[0].SpecificCase + "] wrong case";
+                    }
+                    else
+                    {
+                        pbtspell = "";
+                    }
+
+                }
+
+                var notes = terms.Notes;
+                var denials = terms.Denials;
+                var gloss = "";
+                List<string> verselist = new List<string>();
+
+                // check against the BiblicalTermsList
+                var bt = _biblicalTermsList.Term.FindAll(t => t.Id == source);
+                if (bt.Count > 0)
+                {
+                    gloss = bt[0].Gloss;
+
+                    foreach (var verse in bt[0].References.Verse)
+                    {
+                        verselist.Add(verse);
+                    }
+
+                    _thedata.Add(new PinsDataTable
+                    {
+                        Code = "KeyTerm",
+                        Gloss = gloss,
+                        Lang = "",
+                        Lform = "",
+                        Match = "KeyTerm" + source,
+                        Notes = "",
+                        Phrase = "",
+                        Prefix = "",
+                        Refs = "",
+                        SimpRefs = "",
+                        Source = target + pbtspell,
+                        Stem = "",
+                        Suffix = "",
+                        Word = "",
+                    });
+                }
+                else
+                {
+                    // now check AllBiblicalTerms
+                    var abt = _allBiblicalTermsList.Term.FindAll(t => t.Id == source);
+                    if (abt.Count > 0)
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        _thedata.Add(new PinsDataTable
+                        {
+                            Code = "KeyTerm",
+                            Gloss = gloss,
+                            Lang = "",
+                            Lform = "",
+                            Match = "KeyTerm" + source,
+                            Notes = "",
+                            Phrase = "",
+                            Prefix = "",
+                            Refs = "",
+                            SimpRefs = "",
+                            Source = target + pbtspell,
+                            Stem = "",
+                            Suffix = "",
+                            Word = "",
+                        });
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
 
             base.OnViewReady(view);
         }
@@ -180,6 +372,30 @@ namespace ClearDashboard.Wpf.ViewModels
 
         #region Methods
 
+        private string CorrectUnicode(string instr)
+        {                                       
+            // There is a problem in Gk Unicode Vowels exhibiting in Paratext. See https://wiki.digitalclassicist.org/Greek_Unicode_duplicated_vowels
+            // The basic code is preferred by the Unicode Consortium
+            // AllBiblicalTerms.xml and Biblical Terms.xml tend to use the Extended codes while TermRenderings.xml tends to use the Basic codes
+            //  Unicode Basic   Extend
+            instr = instr.Replace('ά', 'ά')     //  ά 	    03AC 	1F71
+                .Replace('έ', 'έ')     //  έ 	    03AD 	1F73
+                .Replace('ή', 'ή')     //  ή 	    03AE 	1F75
+                .Replace('ί', 'ί')     //  ί 	    03AF 	1F77
+                .Replace('ό', 'ό')     //  ό 	    03CC 	1F79
+                .Replace('ύ', 'ύ')     //  ύ 	    03CD 	1F7B
+                .Replace('ώ', 'ώ')     //  ώ 	    03CE 	1F7D
+                .Replace('Ά', 'Ά')     //  Ά 	    0386 	1FBB
+                .Replace('Έ', 'Έ')     //  Έ 	    0388 	1FC9
+                .Replace('Ή', 'Ή')     //  Ή 	    0389 	1FCB
+                .Replace('Ί', 'Ί')     //  Ί 	    038A 	1FDB
+                .Replace('Ό', 'Ό')     //  Ό 	    038C 	1FF9
+                .Replace('Ύ', 'Ύ')     //  Ύ 	    038E 	1FEB
+                .Replace('Ώ', 'Ώ')     //  Ώ 	    038F 	1FFB
+                .Replace('ΐ', 'ΐ')     //  ΐ 	    0390 	1FD3
+                .Replace('ΰ', 'ΰ');    //  ΰ 	    03B0 	1FE3
+            return instr;
+        }
 
         #endregion // Methods
     }
@@ -339,6 +555,45 @@ namespace ClearDashboard.Wpf.ViewModels
         [XmlText]
         public string Text { get; set; }
     }
+
+
+
+
+    // using System.Xml.Serialization;
+    // XmlSerializer serializer = new XmlSerializer(typeof(SpellingStatus));
+    // using (StringReader reader = new StringReader(xml))
+    // {
+    //    var test = (SpellingStatus)serializer.Deserialize(reader);
+    // }
+
+    [XmlRoot(ElementName = "Status")]
+    public class Status
+    {
+
+        [XmlAttribute(AttributeName = "Word")]
+        public string Word { get; set; }
+
+        [XmlAttribute(AttributeName = "State")]
+        public string State { get; set; }
+
+        [XmlElement(ElementName = "SpecificCase")]
+        public string SpecificCase { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+
+        [XmlElement(ElementName = "Correction")]
+        public string Correction { get; set; }
+    }
+
+    [XmlRoot(ElementName = "SpellingStatus")]
+    public class SpellingStatus
+    {
+
+        [XmlElement(ElementName = "Status")]
+        public List<Status> Status { get; set; }
+    }
+
 
 
 }
