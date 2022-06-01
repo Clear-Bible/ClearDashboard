98cb5d6e00a4cd07b8623ccc4e595a485b099b05
GeoDirk <geodirk@gmail.com>
2022-06-01 16:29:45 -0400

    migrated the models for the XML files to the right DAL directory

---
+++ src/ClearDashboard.DAL.Models/Common/SpellingStatus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ClearDashboard.DataAccessLayer.Models.Common
+{
+    [XmlRoot(ElementName = "Status")]
+    public class Status
+    {
+
+        [XmlAttribute(AttributeName = "Word")]
+        public string Word { get; set; }
+
+        [XmlAttribute(AttributeName = "State")]
+        public string State { get; set; }
+
+        [XmlElement(ElementName = "SpecificCase")]
+        public string SpecificCase { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+
+        [XmlElement(ElementName = "Correction")]
+        public string Correction { get; set; }
+    }
+
+    [XmlRoot(ElementName = "SpellingStatus")]
+    public class SpellingStatus
+    {
+
+        [XmlElement(ElementName = "Status")]
+        public List<Status> Status { get; set; }
+    }
+}
---
+++ src/ClearDashboard.DAL.Models/Common/TermRendering.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ClearDashboard.DataAccessLayer.Models.Common
+{
+
+    [XmlRoot(ElementName = "TermRendering")]
+    public class TermRendering
+    {
+
+        [XmlElement(ElementName = "Changes")]
+        public Changes Changes { get; set; }
+
+        [XmlElement(ElementName = "Notes")]
+        public object Notes { get; set; }
+
+        [XmlElement(ElementName = "Denials")]
+        public Denials Denials { get; set; }
+
+        [XmlAttribute(AttributeName = "Id")]
+        public string Id { get; set; }
+
+        [XmlAttribute(AttributeName = "Guess")]
+        public bool Guess { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+
+        [XmlElement(ElementName = "Renderings")]
+        public string Renderings { get; set; }
+
+        [XmlElement(ElementName = "Glossary")]
+        public object Glossary { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Change")]
+    public class Change
+    {
+
+        [XmlElement(ElementName = "Before")]
+        public object Before { get; set; }
+
+        [XmlElement(ElementName = "After")]
+        public string After { get; set; }
+
+        [XmlAttribute(AttributeName = "UserName")]
+        public string UserName { get; set; }
+
+        [XmlAttribute(AttributeName = "TermId")]
+        public string TermId { get; set; }
+
+        [XmlAttribute(AttributeName = "Date")]
+        public DateTime Date { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Changes")]
+    public class Changes
+    {
+
+        [XmlElement(ElementName = "Change")]
+        public List<Change> Change { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Denials")]
+    public class Denials
+    {
+
+        [XmlElement(ElementName = "Denial")]
+        public List<int> Denial { get; set; }
+    }
+
+    [XmlRoot(ElementName = "TermRenderingsList")]
+    public class TermRenderingsList
+    {
+
+        [XmlElement(ElementName = "TermRendering")]
+        public List<TermRendering> TermRendering { get; set; }
+    }
+
+
+}
