2d20effb29e45809e0db8c3f2027b740691cd89c
Michael Gerfen <gerfen@hotmail.com>
2022-06-15 10:00:25 -0700

    Added UserIdValueGenerator to automatically populate UserId when entiies with a UserId property are added.

---
+++ src/ClearDashboard.DAL.Data/ValueGenerators/UserIdValueGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace ClearDashboard.DataAccessLayer.Data.ValueGenerators
+{
+    
+    internal class UserIdValueGenerator : ValueGenerator<Guid>
+    {
+        public override bool GeneratesTemporaryValues => false;
+        public override Guid Next(EntityEntry entry) => GetUserId(entry.Context);
+        Guid GetUserId(DbContext context) => ((AlignmentContext)context).UserProvider.CurrentUser.Id;
+    }
+}
