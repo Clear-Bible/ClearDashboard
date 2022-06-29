f665bc53411639c302fd2746b7c9ff73b73c9268
Michael Gerfen <gerfen@hotmail.com>
2022-06-29 16:34:43 -0700

    Added a new project to share command and query handler base classes.

---
+++ src/ClearDashboard.DAL.CQRS.Features/Features/ResourceRequestHandler.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ClearDashboard.DAL.CQRS.Features.Features;
+
+public abstract class ResourceRequestHandler<TRequest, TResponse, TData> : IRequestHandler<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    protected ILogger Logger { get; }
+    protected string ResourceDirectory { get; set; }  = Path.Combine(Environment.CurrentDirectory, "Resources");
+    protected abstract string ResourceName { get; set; }
+    protected string ResourcePath => Path.Combine(ResourceDirectory, ResourceName);
+    
+    protected ResourceRequestHandler(ILogger logger)
+    {
+        Logger = logger;
+    }
+
+    public abstract Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+    protected abstract TData ProcessData();
+
+    protected RequestResult<TResultData> ValidateResourcePath<TResultData>(TResultData data)
+    {
+        var queryResult = new RequestResult<TResultData>(data);
+
+        if (string.IsNullOrEmpty(ResourceName))
+        {
+            LogAndSetUnsuccessfulResult(ref queryResult, "Please set 'ResourceName'.");
+            return queryResult;
+        }
+       
+        if (!File.Exists(ResourcePath) )
+        {
+
+            LogAndSetUnsuccessfulResult(ref queryResult, $"{ResourceName} does not exist in the directory {ResourceDirectory}");
+        }
+        return queryResult;
+    }
+
+    protected void LogAndSetUnsuccessfulResult<TResultData>(ref RequestResult<TResultData> requestResult, string message, Exception? ex = null)
+    {
+        if (ex != null)
+        {
+            Logger.LogError(ex, message);
+            requestResult.Message = string.IsNullOrEmpty(message)? ex.Message: $"{message}. {ex.Message}";
+        }
+        else
+        {
+            Logger.LogError(message);
+            requestResult.Message = message;
+        }
+        requestResult.Success = false;
+    }
+
+}
---
+++ src/ClearDashboard.DAL.CQRS.Features/Features/XmlReaderRequestHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ClearDashboard.DAL.CQRS.Features.Features;
+
+public abstract class XmlReaderRequestHandler<TRequest, TResponse, TData> : ResourceRequestHandler<TRequest, TResponse, TData>
+    where TRequest : IRequest<TResponse>
+{
+    protected XmlReader? XmlReader { get; private set; }
+
+    protected XmlReaderRequestHandler(ILogger logger) : base(logger)
+    {
+        //no-op
+    }
+
+    
+    protected TData LoadXmlAndProcessData(XmlReaderSettings? xmlReaderSettings = null)
+    {
+        try
+        {
+            XmlReader = xmlReaderSettings != null ? XmlReader.Create(ResourcePath, xmlReaderSettings): XmlReader.Create(ResourcePath);
+            return ProcessData();
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, $"An unexpected error occurred while processing the XML file: '{ResourcePath}'");
+            throw;
+        }
+        finally
+        {
+            XmlReader?.Dispose();
+        }
+    }
+}
