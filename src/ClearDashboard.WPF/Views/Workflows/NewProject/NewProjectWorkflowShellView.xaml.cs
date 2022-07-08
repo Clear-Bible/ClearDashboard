232f35477748f464a4d0e8d8e9b8d64fab95161e
Michael Gerfen <gerfen@hotmail.com>
2022-07-08 14:23:00 -0700

    WIP:  New Project UI

---
+++ src/ClearDashboard.WPF/Views/Workflows/NewProject/NewProjectWorkflowShellView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Views.Workflows.NewProject
+{
+    /// <summary>
+    /// Interaction logic for NewProjectWorkflowShellView.xaml
+    /// </summary>
+    public partial class NewProjectWorkflowShellView : Page
+    {
+        public NewProjectWorkflowShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
