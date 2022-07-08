a857fabd6fb4bc407b158168a4ff2f15c8eaca52
Christopher Morley <chris.morley@clear.bible>
2022-07-08 10:53:39 -0700

    GetBookIds handler and test

--- src/ClearDashboard.DAL.Alignment/Features/Corpora/GetBookIdsByTokenizedCorpusIdQueryHandler.cs
+++ src/ClearDashboard.DAL.Alignment/Features/Corpora/GetBookIdsByTokenizedCorpusIdQueryHandler.cs
@@ -1,53 +1,74 @@
-﻿using System.Text;
+﻿using ClearBible.Engine.Corpora;
+using ClearBible.Engine.Persistence;
+using System.Text;
 using ClearDashboard.DAL.Alignment.Corpora;
 using ClearDashboard.DAL.CQRS;
 using ClearDashboard.DAL.CQRS.Features;
 using ClearDashboard.DAL.Interfaces;
 using ClearDashboard.DataAccessLayer.Data;
+using MediatR;
 using Microsoft.Extensions.Logging;
-using SIL.Machine.Corpora;
-
+using SIL.Extensions;
 
 //USE TO ACCESS Models
 using Models = ClearDashboard.DataAccessLayer.Models;
 
-namespace ClearDashboard.DAL.Alignment.Features.Corpora
+namespace ClearDashboard.DAL.Alignment.Features.Corpora;
+
+public class GetBookIdsByTokenizedCorpusIdQueryHandler : ProjectDbContextQueryHandler<
+    GetBookIdsByTokenizedCorpusIdQuery,
+    RequestResult<(IEnumerable<string> bookId, CorpusId corpusId)>,
+    (IEnumerable<string> bookId, CorpusId corpusId)>
 {
-    public class GetBookIdsByTokenizedCorpusIdQueryHandler : ProjectDbContextQueryHandler<
-        GetBookIdsByTokenizedCorpusIdQuery,
-        RequestResult<(IEnumerable<string> bookId, CorpusId corpusId)>,
-        (IEnumerable<string> bookId, CorpusId corpusId)>
+    private readonly IMediator _mediator;
+
+    public GetBookIdsByTokenizedCorpusIdQueryHandler(IMediator mediator,
+        ProjectDbContextFactory? projectNameDbContextFactory, IProjectProvider projectProvider,
+        ILogger<GetBookIdsByTokenizedCorpusIdQueryHandler> logger)
+        : base(projectNameDbContextFactory, projectProvider, logger)
     {
-        // **********************************************
-        // TODO:  Remove the following two lines
-        public static readonly string TestDataPath = Path.Combine(AppContext.BaseDirectory,
-            "..", "..", "..", "Corpora", "data");
-        public static readonly string UsfmTestProjectPath = Path.Combine(TestDataPath, "usfm", "Tes");
-        // **********************************************
-
-        public GetBookIdsByTokenizedCorpusIdQueryHandler(ProjectDbContextFactory? projectNameDbContextFactory, IProjectProvider projectProvider, ILogger<GetBookIdsByTokenizedCorpusIdQueryHandler> logger) 
-            : base(projectNameDbContextFactory, projectProvider, logger)
-        {
-        }
-
-        protected override async Task<RequestResult<(IEnumerable<string> bookId, CorpusId corpusId)>> GetDataAsync(GetBookIdsByTokenizedCorpusIdQuery request, CancellationToken cancellationToken)
-        {
-            //DB Impl notes: look at command.TokenizedCorpusId and find in TokenizedCorpus table.
-            // pull out its parent CorpusId
-            //Then iterate tokenization.Corpus(parent).Verses(child) and find unique bookAbbreviations and return as IEnumerable<string>
-
-           
-            // TODO: how to get the path here?
-            var corpus = new UsfmFileTextCorpus("usfm.sty", Encoding.UTF8, UsfmTestProjectPath);
-            
-            //var corpus = new UsfmFileTextCorpus("usfm.sty", Encoding.UTF8, TestDataHelpers.UsfmTestProjectPath);
-
-            return await Task.FromResult(
-                new RequestResult<(IEnumerable<string> bookId, CorpusId corpusId)>
-                (result: (corpus.Texts.Select(t => t.Id), new CorpusId(new Guid())),
-                    success: true,
-                    message: "successful result from test"));
-        }
-
+        _mediator = mediator;
     }
+
+    protected override async Task<RequestResult<(IEnumerable<string> bookId, CorpusId corpusId)>> GetDataAsync(
+        GetBookIdsByTokenizedCorpusIdQuery request, CancellationToken cancellationToken)
+    {
+        //DB Impl notes: look at command.TokenizedCorpusId and find in TokenizedCorpus table.
+        // pull out its parent CorpusId
+        //Then iterate tokenization.Corpus(parent).Verses(child) and find unique bookAbbreviations and return as IEnumerable<string>
+        var tokenizedCorpus = ProjectDbContext.TokenizedCorpora.FirstOrDefault(i => i.Id == request.TokenizedCorpusId.Id);
+
+        if (tokenizedCorpus == null)
+        {
+            return new RequestResult<(IEnumerable<string> bookId, CorpusId corpusId)>
+                (
+                    result: (new List<string>(), new CorpusId(new Guid())),
+                    success: false,
+                    message: $"TokenizedCorpus not found for TokenizedCorpusId {request.TokenizedCorpusId.Id}"
+                );
+        }
+
+        var bookNumbers = tokenizedCorpus.Tokens.GroupBy(token => token.BookNumber).Select(g => g.Key);
+        var bookIdsToAbbreviations = FileGetBookIds.BookIds.ToDictionary(x => int.Parse(x.silCannonBookNum), x => x.silCannonBookAbbrev);
+
+        var bookAbbreviations = new List<string>();
+        foreach (var bookNumber in bookNumbers)
+        {
+            if (!bookIdsToAbbreviations.TryGetValue(bookNumber ?? -1, out string? bookAbbreviation))
+            {
+                return new RequestResult<(IEnumerable<string> bookId, CorpusId corpusId)>
+                    (
+                        result: (new List<string>(), new CorpusId(new Guid())),
+                        success: false,
+                        message: $"Book number '{bookNumber}' not found in FileGetBooks.BookIds"
+                    );
+            }
+
+            bookAbbreviations.Add(bookAbbreviation);
+        }
+
+        return new RequestResult<(IEnumerable<string> bookId, CorpusId corpusId)>
+            ((bookAbbreviations, new CorpusId(tokenizedCorpus.Corpus.Id)));
+    }
+
 }
