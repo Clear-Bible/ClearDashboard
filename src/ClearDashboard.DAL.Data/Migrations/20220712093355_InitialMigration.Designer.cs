6766d09ee9888cb7d791b5a25a39cee351f88fc6
Michael Gerfen <gerfen@hotmail.com>
2022-07-12 02:34:45 -0700

    Regenerated migrations after merging in main. to picked Mike's model changes.

--- src/ClearDashboard.DAL.Data/Migrations/20220712093355_InitialMigration.Designer.cs
+++ src/ClearDashboard.DAL.Data/Migrations/20220712093355_InitialMigration.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClearDashboard.DataAccessLayer.Data.Migrations
 {
     [DbContext(typeof(ProjectDbContext))]
-    [Migration("20220712092823_InitialMigration")]
+    [Migration("20220712093355_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
