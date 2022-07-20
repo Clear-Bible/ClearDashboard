d269f485dba55e13d144ae95e5f186d5a25db8b9
Michael Gerfen <gerfen@hotmail.com>
2022-07-20 09:58:24 -0700

    WIP:  new project dialog

---
+++ src/ClearDashboard.DAL.Models/Paratext/ParatextProjectMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearDashboard.DataAccessLayer.Models.Paratext
+{
+    public class ParatextProjectMetadata
+    {
+        public string Id { get; set; }
+        public ProjectType ProjectType { get; set; }
+        public CorpusType CorpusType { get; set; }
+        public string Name { get; set; }
+        public string LongName { get; set; }
+        public string LanguageName { get; set; }
+    }
+}
