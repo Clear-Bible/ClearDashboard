a9f2dd96706e658952b2be1058073ca4b3d45e47
Michael Gerfen <gerfen@hotmail.com>
2022-07-20 17:49:06 -0700

    Aligned CorpusType with Paratext CorpusType to make casting between enums more stright forward.

--- src/ClearDashboard.DAL.Models/Common/CorpusType.cs
+++ src/ClearDashboard.DAL.Models/Common/CorpusType.cs
@@ -2,23 +2,38 @@
 
 public enum CorpusType
 {
-    Unknown,
+    /// <summary>A standard translation with no base project</summary>
     Standard,
-    Resource,
+    /// <summary>A back translation of another translation</summary>
     BackTranslation,
+    /// <summary>A translation based on a front translation</summary>
     Daughter,
+    /// <summary>A manual transliteration of a text</summary>
+    TransliterationManual,
+    /// <summary>An automated transliteration of a text</summary>
+    TransliterationWithEncoder,
+    /// <summary>A study Bible based on an original text</summary>
+    StudyBible,
+    /// <summary>Collection of consultant notes</summary>
+    ConsultantNotes,
+    /// <summary>A study Bible additions project</summary>
+    StudyBibleAdditions,
+    /// <summary>A standard project that goes with a base project used for anything related to the project that
+    /// doesn't fit into another derived type.</summary>
+    Auxiliary,
+    /// <summary>A project stored in xml format</summary>
+    Xml,
+    /// <summary>A source language project</summary>
+    SourceLanguage,
+    /// <summary>A dictionary project</summary>
+    Dictionary,
+    /// <summary>NB:  this is an Enhanced Resource in Paratext - a published resource that has text mapped to Hebrew/Greek source text.</summary>
     MarbleResource,
 
-    TransliterationManual,
-    TransliterationWithEncoder,
-    StudyBible,
-    ConsultantNotes,
-    StudyBibleAdditions,
-    Auxiliary,
-    Xml,
-    SourceLanguage,
-    Dictionary,
-    EnhancedResource,
+
+    // NB:  CODE REVIEW:  Do we still need this WRT to ParatextProxy lines 264 & 439, CreateNewProjectWorkflowStepViewModel line 315?
+    Resource,
+    Unknown
 }
 
 public enum CorpusSourceType
