a8af95fe47102e86b16c48aa812269956c60fb64
Michael Gerfen <gerfen@hotmail.com>
2022-07-20 17:50:13 -0700

    WIP:  New project changes

--- src/ClearDashboard.DAL.Models/Paratext/ParatextProjectMetadata.cs
+++ src/ClearDashboard.DAL.Models/Paratext/ParatextProjectMetadata.cs
@@ -4,10 +4,11 @@ namespace ClearDashboard.DataAccessLayer.Models
     public class ParatextProjectMetadata
     {
         public string Id { get; set; }
-        public ProjectType ProjectType { get; set; }
         public CorpusType CorpusType { get; set; }
         public string Name { get; set; }
         public string LongName { get; set; }
         public string LanguageName { get; set; }
+
+        public string CorpusTypeDisplay => CorpusType.ToString();
     }
 }
