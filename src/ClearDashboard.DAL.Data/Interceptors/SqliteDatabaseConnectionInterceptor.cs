542e9ec1a86014869389691d786178a4cb3576d7
Michael Gerfen <gerfen@hotmail.com>
2022-08-12 10:19:11 -0700

    Added an interceptor in an attempt to change the connection string of an exisiting database.

---
+++ src/ClearDashboard.DAL.Data/Interceptors/SqliteDatabaseConnectionInterceptor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Data.Common;
+namespace ClearDashboard.DataAccessLayer.Data.Interceptors
+{
+
+
+
+
+    public class SqliteDatabaseConnectionInterceptor : DbConnectionInterceptor
+    {
+        public SqliteDatabaseConnectionInterceptor(string databaseName)
+        {
+            database = databaseName;
+        }
+
+        readonly string database;
+
+        public override void ConnectionOpened(DbConnection connection, ConnectionEndEventData eventData)
+        {
+            if (database != null)
+            {
+                connection.ChangeDatabase(database); // The 'magic' code
+            }
+            base.ConnectionOpened(connection, eventData);
+        }
+
+        public override async Task ConnectionOpenedAsync(DbConnection connection, ConnectionEndEventData eventData, CancellationToken cancellationToken = default)
+        {
+            if (database != null)
+            {
+                await connection.ChangeDatabaseAsync(database); // The 'magic' code
+            }
+            await base.ConnectionOpenedAsync(connection, eventData, cancellationToken);
+        }
+
+    }
+}
