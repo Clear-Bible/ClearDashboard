4cd20039c7628e5dde83b7b92f247b68b2617df5
Robertson Brinker <60048070+robertsonbrinker@users.noreply.github.com>
2022-08-12 09:17:05 -0400

    bare bones test

---
+++ src/ClearDashboard.WPF.Tests/DetectParatextRunningConverterTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ClearDashboard.WPF;
+using System.Diagnostics;
+
+namespace ClearDashboard.WPF.Tests
+{
+    public class DetectParatextRunningConverterTest
+    {
+        public DetectParatextRunningConverterTest() {
+
+        }
+
+        [Fact]
+        public void ParatextDetectionTest() 
+        { 
+        }
+
+        public void IsParatextRunning()
+        {
+            if (Process.GetProcessesByName("Paratext").Length > 0)
+            {
+                // stuff to do while paratext is running 
+                System.Console.WriteLine("yo");
+            }
+        }
+
+    }
+}
