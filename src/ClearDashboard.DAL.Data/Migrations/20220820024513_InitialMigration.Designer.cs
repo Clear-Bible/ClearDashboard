452bc315d8266832ccdb4f744a2b05724e4c0823
Christopher Morley <chris.morley@clear.bible>
2022-08-19 19:45:57 -0700

    Make sure initial migration matches db model

--- src/ClearDashboard.DAL.Data/Migrations/20220820024513_InitialMigration.Designer.cs
+++ src/ClearDashboard.DAL.Data/Migrations/20220820024513_InitialMigration.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClearDashboard.DataAccessLayer.Data.Migrations
 {
     [DbContext(typeof(ProjectDbContext))]
-    [Migration("20220818212057_InitialMigration")]
+    [Migration("20220820024513_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
