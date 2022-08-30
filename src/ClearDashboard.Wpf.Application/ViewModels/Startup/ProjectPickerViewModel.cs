10ea7f4acadc4f5d2752b0cf22159192a420f941
Robertson Brinker <60048070+robertsonbrinker@users.noreply.github.com>
2022-08-30 16:56:35 -0400

    startup UserControls working

---
+++ src/ClearDashboard.Wpf.Application/ViewModels/Startup/ProjectPickerViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Caliburn.Micro;
+using ClearApplicationFoundation.ViewModels.Infrastructure;
+using ClearDashboard.Wpf.Application.ViewModels.Main;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClearDashboard.Wpf.Application.ViewModels.Startup
+{
+    internal class ProjectPickerViewModel : WorkflowStepViewModel
+    {
+
+        public ProjectPickerViewModel(IEventAggregator eventAggregator, ILogger<MainViewModel> logger, IMediator mediator, INavigationService navigationService) : base(eventAggregator, navigationService, logger, mediator)
+        {
+
+        }
+
+        protected async Task OnInitializeAsync(CancellationToken cancellationToken)
+        {
+
+            CanMoveForwards = true;
+            CanMoveBackwards = true;
+            EnableControls = true;
+        }
+    }
+}
