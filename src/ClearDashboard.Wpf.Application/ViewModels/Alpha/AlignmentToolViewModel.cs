5f77fd64f36da9e4fcf9d14d0a1d8ddbb21c76cf
GeoDirk <geodirk@gmail.com>
2022-08-31 11:11:23 -0400

    AlignmentTool migration

---
+++ src/ClearDashboard.Wpf.Application/ViewModels/Alpha/AlignmentToolViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Caliburn.Micro;
+using ClearDashboard.DataAccessLayer.Wpf;
+using ClearDashboard.Wpf.Application.ViewModels.Panes;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ClearDashboard.Wpf.Application.ViewModels
+{
+    public class AlignmentToolViewModel : PaneViewModel
+    {
+        #region Member Variables
+
+        #endregion //Member Variables
+
+        #region Public Properties
+
+        public string ContentID => this.ContentID;
+
+        #endregion //Public Properties
+
+        #region Observable Properties
+
+
+
+        #endregion //Observable Properties
+
+        #region Constructor
+        public AlignmentToolViewModel()
+        {
+
+        }
+
+        public AlignmentToolViewModel(INavigationService navigationService, ILogger<AlignmentToolViewModel> logger,
+            DashboardProjectManager projectManager, IEventAggregator eventAggregator, IMediator mediator) :
+            base(navigationService, logger, projectManager, eventAggregator, mediator)
+        {
+            this.Title = "⳼ ALIGNMENT TOOL";
+            this.ContentId = "ALIGNMENTTOOL";
+        }
+
+        #endregion //Constructor
+
+        #region Methods
+
+        #endregion // Methods
+
+
+    }
+}
