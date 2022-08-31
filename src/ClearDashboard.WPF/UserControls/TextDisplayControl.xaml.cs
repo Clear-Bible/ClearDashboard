21d779d145eb24468c89c8269fa0a6afb2833851
Andy Gray <andy.gray@blackcrater.com>
2022-08-31 16:33:32 -0700

    Leverage word display control

--- src/ClearDashboard.WPF/UserControls/TextDisplayControl.xaml.cs
+++ src/ClearDashboard.WPF/UserControls/TextDisplayControl.xaml.cs
@@ -5,70 +5,65 @@ using System.Windows.Media;
 
 namespace ClearDashboard.Wpf.UserControls
 {
-    public class TokenEventArgs : RoutedEventArgs
-    {
-        public string SurfaceText { get; set; }
-    }
-
     /// <summary>
     /// A control for displaying a list of words/tokens.
     /// </summary>
     public partial class TextDisplayControl : UserControl
     {
         #region Static RoutedEvents
-        /// <summary>
-        /// Identifies the TokenClickedEvent routed event.
-        /// </summary>
-        public static readonly RoutedEvent TokenClickedEvent = EventManager.RegisterRoutedEvent
-            ("TokenClicked", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
+        ///// <summary>
+        ///// Identifies the TokenClickedEvent routed event.
+        ///// </summary>
+        //public static readonly RoutedEvent TokenClickedEvent = EventManager.RegisterRoutedEvent
+        //    ("TokenClicked", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
 
-        /// <summary>
-        /// Identifies the TokenDoubleClickedEvent routed event.
-        /// </summary>
-        public static readonly RoutedEvent TokenDoubleClickedEvent = EventManager.RegisterRoutedEvent
-            ("TokenDoubleClicked", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
+        ///// <summary>
+        ///// Identifies the TokenDoubleClickedEvent routed event.
+        ///// </summary>
+        //public static readonly RoutedEvent TokenDoubleClickedEvent = EventManager.RegisterRoutedEvent
+        //    ("TokenDoubleClicked", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
 
-        /// <summary>
-        /// Identifies the TokenLeftButtonDownEvent routed event.
-        /// </summary>
-        public static readonly RoutedEvent TokenLeftButtonDownEvent = EventManager.RegisterRoutedEvent
-            ("TokenLeftButtonDown", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
+        ///// <summary>
+        ///// Identifies the TokenLeftButtonDownEvent routed event.
+        ///// </summary>
+        //public static readonly RoutedEvent TokenLeftButtonDownEvent = EventManager.RegisterRoutedEvent
+        //    ("TokenLeftButtonDown", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
 
-        /// <summary>
-        /// Identifies the TokenLeftButtonUpEvent routed event.
-        /// </summary>
-        public static readonly RoutedEvent TokenLeftButtonUpEvent = EventManager.RegisterRoutedEvent
-            ("TokenLeftButtonUp", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
+        ///// <summary>
+        ///// Identifies the TokenLeftButtonUpEvent routed event.
+        ///// </summary>
+        //public static readonly RoutedEvent TokenLeftButtonUpEvent = EventManager.RegisterRoutedEvent
+        //    ("TokenLeftButtonUp", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
 
-        /// <summary>
-        /// Identifies the TokenRightButtonDownEvent routed event.
-        /// </summary>
-        public static readonly RoutedEvent TokenRightButtonDownEvent = EventManager.RegisterRoutedEvent
-            ("TokenRightButtonDown", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
+        ///// <summary>
+        ///// Identifies the TokenRightButtonDownEvent routed event.
+        ///// </summary>
+        //public static readonly RoutedEvent TokenRightButtonDownEvent = EventManager.RegisterRoutedEvent
+        //    ("TokenRightButtonDown", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
 
-        /// <summary>
-        /// Identifies the TokenRightButtonUpEvent routed event.
-        /// </summary>
-        public static readonly RoutedEvent TokenRightButtonUpEvent = EventManager.RegisterRoutedEvent
-            ("TokenRightButtonUp", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
+        ///// <summary>
+        ///// Identifies the TokenRightButtonUpEvent routed event.
+        ///// </summary>
+        //public static readonly RoutedEvent TokenRightButtonUpEvent = EventManager.RegisterRoutedEvent
+        //    ("TokenRightButtonUp", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
 
-        /// <summary>
-        /// Identifies the TokenMouseEnterEvent routed event.
-        /// </summary>
-        public static readonly RoutedEvent TokenMouseEnterEvent = EventManager.RegisterRoutedEvent
-            ("TokenMouseEnter", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
+        ///// <summary>
+        ///// Identifies the TokenMouseEnterEvent routed event.
+        ///// </summary>
+        //public static readonly RoutedEvent TokenMouseEnterEvent = EventManager.RegisterRoutedEvent
+        //    ("TokenMouseEnter", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
 
-        /// <summary>
-        /// Identifies the TokenMouseLeaveEvent routed event.
-        /// </summary>
-        public static readonly RoutedEvent TokenMouseLeaveEvent = EventManager.RegisterRoutedEvent
-            ("TokenMouseLeave", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
+        ///// <summary>
+        ///// Identifies the TokenMouseLeaveEvent routed event.
+        ///// </summary>
+        //public static readonly RoutedEvent TokenMouseLeaveEvent = EventManager.RegisterRoutedEvent
+        //    ("TokenMouseLeave", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
 
-        /// <summary>
-        /// Identifies the TokenMouseWheelEvent routed event.
-        /// </summary>
-        public static readonly RoutedEvent TokenMouseWheelEvent = EventManager.RegisterRoutedEvent
-            ("TokenMouseWheel", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
+        ///// <summary>
+        ///// Identifies the TokenMouseWheelEvent routed event.
+        ///// </summary>
+        //public static readonly RoutedEvent TokenMouseWheelEvent = EventManager.RegisterRoutedEvent
+        //    ("TokenMouseWheel", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TextDisplayControl));
 
         #endregion Static RoutedEvents
         #region Static DependencyProperties
@@ -86,8 +81,8 @@ namespace ClearDashboard.Wpf.UserControls
         /// <summary>
         /// Identifies the InnerMargin dependency property.
         /// </summary>
-        public static readonly DependencyProperty InnerMarginProperty = DependencyProperty.Register("InnerMargin", typeof(Thickness), typeof(TextDisplayControl),
-            new PropertyMetadata(new Thickness(6, 2, 6, 2)));
+        //public static readonly DependencyProperty InnerMarginProperty = DependencyProperty.Register("InnerMargin", typeof(Thickness), typeof(TextDisplayControl),
+        //    new PropertyMetadata(new Thickness(6, 2, 6, 2)));
 
         /// <summary>
         /// Identifies the Wrap dependency property.
@@ -122,15 +117,15 @@ namespace ClearDashboard.Wpf.UserControls
         /// <summary>
         /// Gets or sets the margin around each word for text display.
         /// </summary>
-        public Thickness InnerMargin
-        {
-            get
-            {
-                var result = GetValue(InnerMarginProperty);
-                return (Thickness) result;
-            }
-            set => SetValue(InnerMarginProperty, value);
-        }
+        //public Thickness InnerMargin
+        //{
+        //    get
+        //    {
+        //        var result = GetValue(InnerMarginProperty);
+        //        return (Thickness) result;
+        //    }
+        //    set => SetValue(InnerMarginProperty, value);
+        //}
 
         /// <summary>
         /// Gets or sets whether the words should wrap in the control.
@@ -162,86 +157,86 @@ namespace ClearDashboard.Wpf.UserControls
         private Thickness StackPadding = new(10,2,10,2);
         public Thickness InnerPadding => ParagraphMode ? ParagraphPadding : StackPadding;
 
-        /// <summary>
-        /// Occurs when an individual token is clicked.
-        /// </summary>
-        public event RoutedEventHandler TokenClicked
-        {
-            add => AddHandler(TokenClickedEvent, value);
-            remove => RemoveHandler(TokenClickedEvent, value);
-        }
+        ///// <summary>
+        ///// Occurs when an individual token is clicked.
+        ///// </summary>
+        //public event RoutedEventHandler TokenClicked
+        //{
+        //    add => AddHandler(TokenClickedEvent, value);
+        //    remove => RemoveHandler(TokenClickedEvent, value);
+        //}
 
-        /// <summary>
-        /// Occurs when an individual token is clicked two or more times.
-        /// </summary>
-        public event RoutedEventHandler TokenDoubleClicked
-        {
-            add => AddHandler(TokenDoubleClickedEvent, value);
-            remove => RemoveHandler(TokenDoubleClickedEvent, value);
-        }
+        ///// <summary>
+        ///// Occurs when an individual token is clicked two or more times.
+        ///// </summary>
+        //public event RoutedEventHandler TokenDoubleClicked
+        //{
+        //    add => AddHandler(TokenDoubleClickedEvent, value);
+        //    remove => RemoveHandler(TokenDoubleClickedEvent, value);
+        //}
 
-        /// <summary>
-        /// Occurs when the left mouse button is pressed while the mouse pointer is over a token.
-        /// </summary>
-        public event RoutedEventHandler TokenLeftButtonDown
-        {
-            add => AddHandler(TokenLeftButtonDownEvent, value);
-            remove => RemoveHandler(TokenLeftButtonDownEvent, value);
-        }
+        ///// <summary>
+        ///// Occurs when the left mouse button is pressed while the mouse pointer is over a token.
+        ///// </summary>
+        //public event RoutedEventHandler TokenLeftButtonDown
+        //{
+        //    add => AddHandler(TokenLeftButtonDownEvent, value);
+        //    remove => RemoveHandler(TokenLeftButtonDownEvent, value);
+        //}
 
-        /// <summary>
-        /// Occurs when the left mouse button is released while the mouse pointer is over a token.
-        /// </summary>
-        public event RoutedEventHandler TokenLeftButtonUp
-        {
-            add => AddHandler(TokenLeftButtonUpEvent, value);
-            remove => RemoveHandler(TokenLeftButtonUpEvent, value);
-        }
+        ///// <summary>
+        ///// Occurs when the left mouse button is released while the mouse pointer is over a token.
+        ///// </summary>
+        //public event RoutedEventHandler TokenLeftButtonUp
+        //{
+        //    add => AddHandler(TokenLeftButtonUpEvent, value);
+        //    remove => RemoveHandler(TokenLeftButtonUpEvent, value);
+        //}
 
-        /// <summary>
-        /// Occurs when the right mouse button is pressed while the mouse pointer is over a token.
-        /// </summary>
-        public event RoutedEventHandler TokenRightButtonDown
-        {
-            add => AddHandler(TokenRightButtonDownEvent, value);
-            remove => RemoveHandler(TokenRightButtonDownEvent, value);
-        }
+        ///// <summary>
+        ///// Occurs when the right mouse button is pressed while the mouse pointer is over a token.
+        ///// </summary>
+        //public event RoutedEventHandler TokenRightButtonDown
+        //{
+        //    add => AddHandler(TokenRightButtonDownEvent, value);
+        //    remove => RemoveHandler(TokenRightButtonDownEvent, value);
+        //}
 
-        /// <summary>
-        /// Occurs when the right mouse button is released while the mouse pointer is over a token.
-        /// </summary>
-        public event RoutedEventHandler TokenRightButtonUp
-        {
-            add => AddHandler(TokenRightButtonUpEvent, value);
-            remove => RemoveHandler(TokenRightButtonUpEvent, value);
-        }
+        ///// <summary>
+        ///// Occurs when the right mouse button is released while the mouse pointer is over a token.
+        ///// </summary>
+        //public event RoutedEventHandler TokenRightButtonUp
+        //{
+        //    add => AddHandler(TokenRightButtonUpEvent, value);
+        //    remove => RemoveHandler(TokenRightButtonUpEvent, value);
+        //}
 
-        /// <summary>
-        /// Occurs when the mouse pointer enters the bounds of a token.
-        /// </summary>
-        public event RoutedEventHandler TokenMouseEnter
-        {
-            add => AddHandler(TokenMouseEnterEvent, value);
-            remove => RemoveHandler(TokenMouseEnterEvent, value);
-        }
+        ///// <summary>
+        ///// Occurs when the mouse pointer enters the bounds of a token.
+        ///// </summary>
+        //public event RoutedEventHandler TokenMouseEnter
+        //{
+        //    add => AddHandler(TokenMouseEnterEvent, value);
+        //    remove => RemoveHandler(TokenMouseEnterEvent, value);
+        //}
 
-        /// <summary>
-        /// Occurs when the mouse pointer leaves the bounds of a token.
-        /// </summary>
-        public event RoutedEventHandler TokenMouseLeave
-        {
-            add => AddHandler(TokenMouseLeaveEvent, value);
-            remove => RemoveHandler(TokenMouseLeaveEvent, value);
-        }
+        ///// <summary>
+        ///// Occurs when the mouse pointer leaves the bounds of a token.
+        ///// </summary>
+        //public event RoutedEventHandler TokenMouseLeave
+        //{
+        //    add => AddHandler(TokenMouseLeaveEvent, value);
+        //    remove => RemoveHandler(TokenMouseLeaveEvent, value);
+        //}
 
-        /// <summary>
-        /// Occurs when the user rotates the mouse wheel while the mouse pointer is over a token.
-        /// </summary>
-        public event RoutedEventHandler TokenMouseWheel
-        {
-            add => AddHandler(TokenMouseWheelEvent, value);
-            remove => RemoveHandler(TokenMouseWheelEvent, value);
-        }
+        ///// <summary>
+        ///// Occurs when the user rotates the mouse wheel while the mouse pointer is over a token.
+        ///// </summary>
+        //public event RoutedEventHandler TokenMouseWheel
+        //{
+        //    add => AddHandler(TokenMouseWheelEvent, value);
+        //    remove => RemoveHandler(TokenMouseWheelEvent, value);
+        //}
 
         public TextDisplayControl()
         {
@@ -260,46 +255,46 @@ namespace ClearDashboard.Wpf.UserControls
 
         private void OnTokenClicked(object sender, RoutedEventArgs e)
         {
-            RaiseTokenEvent(TokenClickedEvent, e);
+//            RaiseTokenEvent(TokenClickedEvent, e);
         }        
         
         private void OnTokenDoubleClicked(object sender, RoutedEventArgs e)
         {
-            RaiseTokenEvent(TokenDoubleClickedEvent, e);
+//            RaiseTokenEvent(TokenDoubleClickedEvent, e);
         }        
         
         private void OnTokenLeftButtonDown(object sender, RoutedEventArgs e)
         {
-            RaiseTokenEvent(TokenLeftButtonDownEvent, e);
+//            RaiseTokenEvent(TokenLeftButtonDownEvent, e);
         }        
         
         private void OnTokenLeftButtonUp(object sender, RoutedEventArgs e)
         {
-            RaiseTokenEvent(TokenRightButtonUpEvent, e);
+//            RaiseTokenEvent(TokenRightButtonUpEvent, e);
         }        
         private void OnTokenRightButtonDown(object sender, RoutedEventArgs e)
         {
-            RaiseTokenEvent(TokenRightButtonDownEvent, e);
+//            RaiseTokenEvent(TokenRightButtonDownEvent, e);
         }        
         
         private void OnTokenRightButtonUp(object sender, RoutedEventArgs e)
         {
-            RaiseTokenEvent(TokenRightButtonUpEvent, e);
+//            RaiseTokenEvent(TokenRightButtonUpEvent, e);
         }        
         
         private void OnTokenMouseEnter(object sender, RoutedEventArgs e)
         {
-            RaiseTokenEvent(TokenMouseEnterEvent, e);
+//            RaiseTokenEvent(TokenMouseEnterEvent, e);
         }
 
         private void OnTokenMouseLeave(object sender, RoutedEventArgs e)
         {
-            RaiseTokenEvent(TokenMouseLeaveEvent, e);
+//            RaiseTokenEvent(TokenMouseLeaveEvent, e);
         }
 
         private void OnTokenMouseWheel(object sender, RoutedEventArgs e)
         {
-            RaiseTokenEvent(TokenMouseWheelEvent, e);
+//            RaiseTokenEvent(TokenMouseWheelEvent, e);
         }
     }
 }
