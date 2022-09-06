b5a073c388d449ec42396d8f0c075f943559a18f
GeoDirk <geodirk@gmail.com>
2022-09-06 14:37:16 -0400

    start of progress circle on shellview - UI is frozen so not showing

--- src/ClearDashboard.Wpf.Application/Views/Shell/ShellView.xaml.cs
+++ src/ClearDashboard.Wpf.Application/Views/Shell/ShellView.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Interop;
 using Caliburn.Micro;
 using ClearDashboard.Wpf.Application.ViewModels.Main;
 using Microsoft.Extensions.Logging;
+using SIL.Progress;
 
 namespace ClearDashboard.Wpf.Application.Views.Shell
 {
@@ -118,6 +119,8 @@ namespace ClearDashboard.Wpf.Application.Views.Shell
 
             // force the background task window to be on top of the bootstraper inserted frame
             Panel.SetZIndex(this.TaskView, 10);
+
+            Panel.SetZIndex(this.ProgressIndicator, 11);
         }
 
         private void Toggle_Checked(object sender, RoutedEventArgs e) => SetTheme();
