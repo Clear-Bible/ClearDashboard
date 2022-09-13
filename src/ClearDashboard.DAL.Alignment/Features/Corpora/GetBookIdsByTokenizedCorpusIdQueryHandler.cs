e4a4554aebc3e4f100bcb2cf5bcae9d9ff125310
Michael Gerfen <gerfen@hotmail.com>
2022-09-13 16:51:04 -0700

    Optimized handler.

--- src/ClearDashboard.DAL.Alignment/Features/Corpora/GetBookIdsByTokenizedCorpusIdQueryHandler.cs
+++ src/ClearDashboard.DAL.Alignment/Features/Corpora/GetBookIdsByTokenizedCorpusIdQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using ClearBible.Engine.Corpora;
+﻿using System.Collections;
+using ClearBible.Engine.Corpora;
 using ClearBible.Engine.Persistence;
 using System.Text;
 using ClearDashboard.DAL.Alignment.Corpora;
@@ -14,8 +15,24 @@ using SIL.Extensions;
 
 //USE TO ACCESS Models
 using Models = ClearDashboard.DataAccessLayer.Models;
+using System.Linq;
 
 namespace ClearDashboard.DAL.Alignment.Features.Corpora;
+
+public class TokenEqualityComparer : IEqualityComparer<Models.Token> {
+    public bool Equals(Models.Token? x, Models.Token? y)
+    {
+        if (x == null || y == null) return false;
+
+        return x.BookNumber == y.BookNumber && x.ChapterNumber == y.ChapterNumber && x.VerseNumber == y.VerseNumber &&
+               x.SubwordNumber == y.SubwordNumber;
+    }
+
+    public int GetHashCode(Models.Token obj)
+    {
+        return obj.EngineTokenId.GetHashCode();
+    }
+}
 
 public class GetBookIdsByTokenizedCorpusIdQueryHandler : ProjectDbContextQueryHandler<
     GetBookIdsByTokenizedCorpusIdQuery,
@@ -38,8 +55,14 @@ public class GetBookIdsByTokenizedCorpusIdQueryHandler : ProjectDbContextQueryHa
         //DB Impl notes: look at command.TokenizedCorpusId and find in TokenizedCorpus table.
         // pull out its parent CorpusId
         //Then iterate tokenization.Corpus(parent).Verses(child) and find unique bookAbbreviations and return as IEnumerable<string>
+        
+        //var tokenizedCorpus =
+        //    ProjectDbContext.TokenizedCorpora.Include(tc => tc.TokenComponents).Include(tc => tc.Corpus).FirstOrDefault(i => i.Id == request.TokenizedTextCorpusId.Id);
+
+
         var tokenizedCorpus =
-            ProjectDbContext.TokenizedCorpora.Include(tc => tc.TokenComponents).Include(tc => tc.Corpus).FirstOrDefault(i => i.Id == request.TokenizedTextCorpusId.Id);
+            ProjectDbContext.TokenizedCorpora.Include(tc => tc.Corpus).FirstOrDefault(i => i.Id == request.TokenizedTextCorpusId.Id);
+
 
         if (tokenizedCorpus == null)
         {
@@ -52,13 +75,20 @@ public class GetBookIdsByTokenizedCorpusIdQueryHandler : ProjectDbContextQueryHa
             );
         }
 
-        var bookNumbers = tokenizedCorpus.Tokens
-            .GroupBy(token => token.BookNumber)
-            .Select(g => g.Key);
+        //var bookNumbers = tokenizedCorpus.Tokens
+        //    .GroupBy(token => token.BookNumber)
+        //    .Select(g => g.Key);
+
+        var bookNumbers = ProjectDbContext.TokenComponents
+            .Where(tc => tc.TokenizationId == request.TokenizedTextCorpusId.Id).Cast<Models.Token>()
+            .Select(tc => tc.BookNumber).Distinct();
+
+
 
         var bookNumbersToAbbreviations =
             FileGetBookIds.BookIds.ToDictionary(x => int.Parse(x.silCannonBookNum),
                 x => x.silCannonBookAbbrev);
+
 
         var bookAbbreviations = new List<string>();
         foreach (var bookNumber in bookNumbers)
