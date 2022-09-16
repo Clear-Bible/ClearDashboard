4878b4b571824a69e64ca80a5ac69f8d6015cd73
Andy Gray <andy.gray@blackcrater.com>
2022-09-16 12:09:28 -0700

    Initial LabelsDisplayControl

---
+++ src/ClearDashboard.Wpf.Application/UserControls/LabelsDisplayControl.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Brush = System.Drawing.Brush;
+using Brushes = System.Drawing.Brushes;
+
+namespace ClearDashboard.Wpf.Application.UserControls
+{
+    /// <summary>
+    /// A control for displaying a list of <see cref="Label"/> values.
+    /// </summary>
+    public partial class LabelsDisplayControl : UserControl
+    {
+        #region Static DependencyProperties
+
+        /// <summary>
+        /// Identifies the Orientation dependency property.
+        /// </summary>
+        public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register("Orientation", typeof(Orientation), typeof(LabelsDisplayControl));
+
+        /// <summary>
+        /// Identifies the LabelMargin dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelMarginProperty = DependencyProperty.Register("LabelMargin", typeof(Thickness), typeof(LabelsDisplayControl),
+            new PropertyMetadata(new Thickness(0, 0, 0, 0)));
+
+        /// <summary>
+        /// Identifies the LabelPadding dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelPaddingProperty = DependencyProperty.Register("LabelPadding", typeof(Thickness), typeof(LabelsDisplayControl),
+            new PropertyMetadata(new Thickness(0, 0, 0, 0)));
+
+        /// <summary>
+        /// Identifies the LabelCornerRadius dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelCornerRadiusProperty = DependencyProperty.Register("LabelCornerRadius", typeof(CornerRadius), typeof(LabelsDisplayControl),
+            new PropertyMetadata(new CornerRadius(0)));
+
+        /// <summary>
+        /// Identifies the LabelBackground dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelBackgroundProperty = DependencyProperty.Register("LabelBackground", typeof(SolidColorBrush), typeof(LabelsDisplayControl)
+            );
+
+        /// <summary>
+        /// Identifies the Labels dependency property.
+        /// </summary>
+        public static readonly DependencyProperty LabelsProperty = DependencyProperty.Register("Labels", typeof(IEnumerable), typeof(LabelsDisplayControl));
+
+        #endregion Static DependencyProperties
+
+        /// <summary>
+        /// Gets or sets the orientation for displaying the labels.
+        /// </summary>
+        public Orientation Orientation
+        {
+            get => (Orientation)GetValue(OrientationProperty);
+            set => SetValue(OrientationProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the margin for individual label boxes.
+        /// </summary>
+        public Thickness LabelMargin
+        {
+            get => (Thickness)GetValue(LabelMarginProperty);
+            set => SetValue(LabelMarginProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the padding for individual label boxes.
+        /// </summary>
+        public Thickness LabelPadding
+        {
+            get => (Thickness)GetValue(LabelPaddingProperty);
+            set => SetValue(LabelPaddingProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the background brush for individual label boxes.
+        /// </summary>
+        public SolidColorBrush LabelBackground
+        {
+            get => (SolidColorBrush)GetValue(LabelBackgroundProperty);
+            set => SetValue(LabelBackgroundProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the corner radius for individual label boxes.
+        /// </summary>
+        public CornerRadius LabelCornerRadius
+        {
+            get => (CornerRadius)GetValue(LabelCornerRadiusProperty);
+            set => SetValue(LabelCornerRadiusProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets a collection of <see cref="Label"/> objects to display in the control.
+        /// </summary>
+        public IEnumerable Labels
+        {
+            get => (IEnumerable)GetValue(LabelsProperty);
+            set => SetValue(LabelsProperty, value);
+        }
+
+        public LabelsDisplayControl()
+        {
+            InitializeComponent();
+            //this.Background = Brushes.White;
+
+        }
+    }
+}
