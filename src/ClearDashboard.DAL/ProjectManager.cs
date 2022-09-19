0623f3d455f81d998cf0a660eb6deacaefb6efe4
Michael Gerfen <gerfen@hotmail.com>
2022-09-19 09:31:32 -0700

    Merge from stash

--- src/ClearDashboard.DAL/ProjectManager.cs
+++ src/ClearDashboard.DAL/ProjectManager.cs
@@ -251,14 +251,19 @@ namespace ClearDashboard.DataAccessLayer
         {
             CreateDashboardProject();
 
+            // Seed the IProjectProvider implementation.
             var projectAssets = await ProjectNameDbContextFactory.Get(projectName);
-
-            CurrentProject = new Project();
-            CurrentProject.ProjectName = projectName;
-            CurrentProject = await CreateProject(projectName);
-
             CurrentDashboardProject.ProjectName = projectAssets.ProjectName;
             CurrentDashboardProject.DirectoryPath = projectAssets.ProjectDirectory;
+
+          
+            CurrentProject = new Project
+            {
+                ProjectName = projectName
+            };
+            CurrentProject = await CreateProject(projectName);
+
+         
            
         }
 
@@ -328,7 +333,17 @@ namespace ClearDashboard.DataAccessLayer
         public async Task<Project> CreateProject(string projectName)
         {
             var result = await ExecuteRequest(new CreateProjectCommand(projectName), CancellationToken.None);
-            return result.Data;
+            if (result.Success)
+            {
+                return result.Data;
+            }
+            else
+            {
+                var message = $"Could not create a project: {result.Message}";
+                Logger.LogError(message);
+                throw new ApplicationException(message);
+            }
+            
         }
 
         public async Task UpdateProject(Project project)
