fbf6dbcce36b9a7de09015a098ada5e2a04649ed
Michael Gerfen <gerfen@hotmail.com>
2022-09-19 23:00:48 -0700

    WIP:  ParallelCorpus dialog - validators

---
+++ src/ClearDashboard.Wpf.Application/Validators/ParallelCorpusValidator.cs
@@ -0,0 +1,24 @@
+﻿using ClearDashboard.DataAccessLayer.Models;
+using FluentValidation;
+using Microsoft.Extensions.Logging;
+
+namespace ClearDashboard.Wpf.Application.Validators;
+
+public class ParallelCorpusValidator : AbstractValidator<ParallelCorpus>
+{
+    private ILogger? _logger;
+
+    public ParallelCorpusValidator(ILogger<ParallelCorpusValidator> logger)
+    {
+
+        RuleFor(x => x.DisplayName).Custom((displayName, context) =>
+        {
+
+            if (string.IsNullOrEmpty(displayName))
+            {
+                return;
+            }
+        });
+    }
+
+}
