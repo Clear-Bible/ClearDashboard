4b742130ec61a5c77aa7823e836052e9e30423dc
Andy Gray <andy.gray@blackcrater.com>
2022-09-20 15:17:32 -0700

    Label margin adjustments

---
+++ src/ClearDashboard.Wpf.Application/UserControls/ThicknessConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace ClearDashboard.Wpf.Application.UserControls
+{
+    public class ThicknessConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new Thickness(System.Convert.ToDouble(values[0]),
+                System.Convert.ToDouble(values[1]),
+                System.Convert.ToDouble(values[2]),
+                System.Convert.ToDouble(values[3]));
+        }
+
+        public object[] ConvertBack(object value, System.Type[] targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
