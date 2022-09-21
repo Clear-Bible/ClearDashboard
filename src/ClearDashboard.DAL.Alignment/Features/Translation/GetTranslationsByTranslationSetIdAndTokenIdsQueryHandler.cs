21b3530c97cb4a512482324813a0dbb1a7640f11
Christopher Morley <chris.morley@clear.bible>
2022-09-20 17:27:43 -0700

    Added first cut of GetTranslations fallback to model

--- src/ClearDashboard.DAL.Alignment/Features/Translation/GetTranslationsByTranslationSetIdAndTokenIdsQueryHandler.cs
+++ src/ClearDashboard.DAL.Alignment/Features/Translation/GetTranslationsByTranslationSetIdAndTokenIdsQueryHandler.cs
@@ -2,6 +2,7 @@
 using ClearDashboard.DAL.CQRS.Features;
 using ClearDashboard.DAL.Interfaces;
 using ClearDashboard.DataAccessLayer.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace ClearDashboard.DAL.Alignment.Features.Translation
@@ -19,20 +20,64 @@ namespace ClearDashboard.DAL.Alignment.Features.Translation
 
         protected override async Task<RequestResult<IEnumerable<Alignment.Translation.Translation>>> GetDataAsync(GetTranslationsByTranslationSetIdAndTokenIdsQuery request, CancellationToken cancellationToken)
         {
+            var translationSet = ProjectDbContext!.TranslationSets
+                .Include(ts => ts.ParallelCorpus)
+                .FirstOrDefault(ts => ts.Id == request.TranslationSetId.Id);
+
+            if (translationSet == null)
+            {
+                return new RequestResult<IEnumerable<Alignment.Translation.Translation>>
+                (
+                    success: false,
+                    message: $"Invalid TranslationSetId '{request.TranslationSetId.Id}' found in request"
+                );
+            }
+
             //var bookNumbers = request.TokenIds.GroupBy(t => t.BookNumber).Select(grp => grp.Key);
-            var tokenIds = request.TokenIds.Select(t => t.Id).ToList();
+            var tokenIdGuids = request.TokenIds.Select(t => t.Id).ToList();
+
             var translations = ProjectDbContext!.Translations
                 .Where(tr => tr.TranslationSetId == request.TranslationSetId.Id)
-                .Where(tr => tokenIds.Contains(tr.SourceTokenComponent!.Id))
+                .Where(tr => tokenIdGuids.Contains(tr.SourceTokenComponent!.Id))
                 .Select(t => new Alignment.Translation.Translation(
                     ModelHelper.BuildToken(t.SourceTokenComponent!),
                     t.TargetText ?? string.Empty,
                     t.TranslationState.ToString()));
 
+            var tokenGuidsNotFound = tokenIdGuids.Except(translations.Select(t => t.SourceToken.TokenId.Id));
+
+            // For any token ids not found in Translations, query the translation model
+            // (by translation set id), join on TokenComponents for tokenGuidsNotFound, 
+            // and create the resulting Translations using the highest TargetTextScores.Text.
+            if (tokenGuidsNotFound.Any())
+            {
+                var translationModelEntries = ProjectDbContext!.TranslationModelEntries
+                    .Include(tm => tm.TargetTextScores)
+                    .Join(
+                        ProjectDbContext!.TokenComponents
+                            .Where(tc => tc.TokenizationId == translationSet.ParallelCorpus!.SourceTokenizedCorpusId)
+                            .Where(tc => tokenGuidsNotFound.Contains(tc.Id)),
+                        tm => tm.SourceText,
+                        tc => tc.TrainingText,
+                        (tm, tc) => new { tm, tc })
+                    .Where(tmtc => tmtc.tm.TranslationSetId == request.TranslationSetId.Id)
+                    .Select(tmtc => new Alignment.Translation.Translation(
+                        ModelHelper.BuildToken(tmtc.tc),
+                        tmtc.tm.TargetTextScores.OrderByDescending(tts => tts.Score).First().Text ?? string.Empty,
+                        "FromTranslationModel"));
+
+                var combined = translations.ToList();
+                combined.AddRange(translationModelEntries.ToList());
+
+                return new RequestResult<IEnumerable<Alignment.Translation.Translation>>(
+                    combined.OrderBy(t => t.SourceToken.TokenId)
+                );
+            }
+
             // need an await to get the compiler to be 'quiet'
             await Task.CompletedTask;
 
-            return new RequestResult<IEnumerable<Alignment.Translation.Translation>>( translations );
+            return new RequestResult<IEnumerable<Alignment.Translation.Translation>>( translations.OrderBy(t => t.SourceToken.TokenId) );
         }
      }
 }
