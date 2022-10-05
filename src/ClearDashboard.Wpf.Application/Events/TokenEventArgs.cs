de957671df2fd6c306010022e2de9e0a060a7964
Andy Gray <andy.gray@blackcrater.com>
2022-10-04 20:33:41 -0700

    Added Intellisense comments

--- src/ClearDashboard.Wpf.Application/Events/TokenEventArgs.cs
+++ src/ClearDashboard.Wpf.Application/Events/TokenEventArgs.cs
@@ -6,7 +6,19 @@ namespace ClearDashboard.Wpf.Application.Events
 {
     public class TokenEventArgs : RoutedEventArgs
     {
+        /// <summary>
+        /// The <see cref="TokenDisplayViewModel"/> that triggered this event.
+        /// </summary>
         public TokenDisplayViewModel TokenDisplayViewModel { get; set; }
+
+        /// <summary>
+        /// The collection of selected <see cref="TokenDisplayViewModel"/>s.
+        /// </summary>
+        public TokenDisplayViewModelCollection SelectedTokens { get; set; } = new();
+
+        /// <summary>
+        /// The keyboard <see cref="ModifierKeys"/> at the time of the event.
+        /// </summary>
         public ModifierKeys ModifierKeys { get; set; }
     }
 }
