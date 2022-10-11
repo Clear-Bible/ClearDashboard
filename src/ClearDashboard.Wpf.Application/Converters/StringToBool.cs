3a83f6bd2276aee652cfe7042018a9bfbf947f41
GeoDirk <geodirk@gmail.com>
2022-10-11 12:40:35 -0400

    cleanup

---
+++ src/ClearDashboard.Wpf.Application/Converters/StringToBool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace ClearDashboard.Wpf.Application.Converters
+{
+    [ValueConversion(typeof(string), typeof(bool))]
+    public class StringToBool : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString() == "True")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
