d0594474176f7ff2d7d4359942c626ef674f463b
Andy Gray <andy.gray@blackcrater.com>
2022-10-28 09:16:58 -0700

    Label collection class

---
+++ src/ClearDashboard.Wpf.Application/ViewModels/Display/LabelCollection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using ClearDashboard.DAL.Alignment.Notes;
+
+namespace ClearDashboard.Wpf.Application.ViewModels.Display
+{
+    public class LabelCollection : ObservableCollection<Label>
+    {
+        public LabelCollection()
+        {
+        }
+
+        public LabelCollection(IEnumerable<Label> labels) : base(labels)
+        {
+        }
+    }
+}
