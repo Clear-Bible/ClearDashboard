d96e8659c301d54694f7e7fbd0a42bfed5f26870
Robertson Brinker <robertsonbrinker@gmail.com>
2022-10-31 16:55:29 -0400

    Temporary fix to speed up pins

--- src/ClearDashboard.Wpf.Application/ViewModels/ParatextViews/PinsViewModel.cs
+++ src/ClearDashboard.Wpf.Application/ViewModels/ParatextViews/PinsViewModel.cs
@@ -593,7 +593,7 @@ namespace ClearDashboard.Wpf.Application.ViewModels.ParatextViews
 
                                     var verseList = datrow.VerseList;
                                     verseList.AddRange(simprefs);
-                                    SortRefs(ref verseList);
+                                    //SortRefs(ref verseList);
                                     datrow.VerseList = verseList;
                                     datrow.SimpRefs = datrow.VerseList.Count.ToString();
                                 }
@@ -608,7 +608,7 @@ namespace ClearDashboard.Wpf.Application.ViewModels.ParatextViews
                     }
                     catch (Exception ex)
                     {
-
+                        Logger.LogError(ex, "Adding in Verse References from Interlinear_*.xml failed");
                     }
                 }
 
