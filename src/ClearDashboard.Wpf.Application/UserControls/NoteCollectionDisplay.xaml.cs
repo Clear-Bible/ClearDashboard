baa606c0b2c33efbb7a065b521271e4175603503
Andy Gray <andy.gray@blackcrater.com>
2022-11-04 09:52:20 -0700

    Collection namespace updates

--- src/ClearDashboard.Wpf.Application/UserControls/NoteCollectionDisplay.xaml.cs
+++ src/ClearDashboard.Wpf.Application/UserControls/NoteCollectionDisplay.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows;
 using System.Windows.Media;
 using ClearDashboard.DAL.Alignment.Notes;
 using ClearDashboard.DataAccessLayer.Annotations;
+using ClearDashboard.Wpf.Application.Collections;
 using ClearDashboard.Wpf.Application.Events;
 using ClearDashboard.Wpf.Application.ViewModels.Display;
 using NotesLabel = ClearDashboard.DAL.Alignment.Notes.Label;
