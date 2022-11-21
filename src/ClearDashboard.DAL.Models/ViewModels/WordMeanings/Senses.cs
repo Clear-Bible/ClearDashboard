8c6c3967cb261206b30602737faddae45ce83b64
GeoDirk <geodirk@gmail.com>
2022-11-21 14:30:17 -0500

    Added Donut Graph Control / view models

---
+++ src/ClearDashboard.DAL.Models/ViewModels/WordMeanings/Senses.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearDashboard.DataAccessLayer.Models.ViewModels.WordMeanings
+{
+    internal class Senses
+    {
+    }
+}
