4302d87a0b58213bf5aea0a08345b03e4036752b
Michael Gerfen <gerfen@hotmail.com>
2022-11-23 16:26:58 -0800

    Using BindableCollection

--- src/ClearDashboard.Wpf.Application/Models/ProjectSerialization/SerializedTokenizedCorpus.cs
+++ src/ClearDashboard.Wpf.Application/Models/ProjectSerialization/SerializedTokenizedCorpus.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using Caliburn.Micro;
 
 namespace ClearDashboard.Wpf.Application.Models.ProjectSerialization;
 
@@ -12,7 +13,7 @@ public class SerializedTokenizedCorpus
     public double X { get; set; }
     public double Y { get; set; }
     public CorpusType CorpusType = CorpusType.Standard;
-    public List<SerializedTokenization> Tokenizations { get;set;} = new();
+    public BindableCollection<SerializedTokenization> Tokenizations { get;set;} = new();
     public Guid CorpusId { get; set; } = Guid.Empty;
     public bool IsRTL { get; set; }
     public string TranslationFontFamily { get; set; } = "Segoe UI";
