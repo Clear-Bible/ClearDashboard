2f30a6466be33673896e8030d619148c7342dc56
Robertson Brinker <robertsonbrinker@gmail.com>
2022-11-30 15:18:14 -0500

    redoing the migration

--- src/ClearDashboard.DAL.Data/Migrations/20221130200538_AddProjectAppVersion.cs
+++ src/ClearDashboard.DAL.Data/Migrations/20221130200538_AddProjectAppVersion.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace ClearDashboard.DataAccessLayer.Data.Migrations
 {
-    public partial class appVersion : Migration
+    public partial class AddProjectAppVersion : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -13,6 +13,11 @@ namespace ClearDashboard.DataAccessLayer.Data.Migrations
                 table: "Project",
                 type: "TEXT",
                 nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "AppVersion", table: "Project");
         }
     }
 }
