d257f590aa6f734561d554ccb52e47ea4a333d26
Michael Gerfen <gerfen@hotmail.com>
2022-12-02 11:24:55 -0800

    Made properties nullable

--- src/ClearDashboard.Wpf.Application/Models/ProjectSerialization/EnhancedViewSerializationModel.cs
+++ src/ClearDashboard.Wpf.Application/Models/ProjectSerialization/EnhancedViewSerializationModel.cs
@@ -7,10 +7,10 @@ namespace ClearDashboard.Wpf.Application.Models.ProjectSerialization
     /// </summary>
     public class EnhancedViewSerializationModel
     {
-        public string Title { get; set; } = string.Empty;
+        public string? Title { get; set; } = string.Empty;
         public bool ParatextSync { get; set; }
-        public string BBBCCCVVV { get; set; } = "001001001";
-        public List<EnhancedViewItemMetadatum> EnhancedViewItems { get; set; } = new List<EnhancedViewItemMetadatum>();
+        public string? BBBCCCVVV { get; set; } = "001001001";
+        public List<EnhancedViewItemMetadatum>? EnhancedViewItems { get; set; } = new List<EnhancedViewItemMetadatum>();
         public int VerseOffset { get; set; }
     }
 }
