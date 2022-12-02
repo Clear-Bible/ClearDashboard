e8a4a45911e0bd27b31189ccec77a5735fa26bf9
Michael Gerfen <gerfen@hotmail.com>
2022-12-02 09:44:57 -0800

    Renamed DisplayOrder to EnhancedViewItemMetadatum

---
+++ src/ClearDashboard.Wpf.Application/Models/ProjectSerialization/EnhancedViewItemMetadatum.cs
@@ -0,0 +1,14 @@
+﻿namespace ClearDashboard.Wpf.Application.Models.ProjectSerialization
+{
+    public class EnhancedViewItemMetadatum
+    {
+        public MessageType MessageType { get; set; }
+        public object? Data { get; set; }
+    }
+
+    public enum MessageType
+    {
+        ShowTokenizationWindowMessage,
+        ShowParallelTranslationWindowMessage
+    }
+}
--- src/ClearDashboard.Wpf.Application/Models/ProjectSerialization/EnhancedViewSerializationModel.cs
+++ src/ClearDashboard.Wpf.Application/Models/ProjectSerialization/EnhancedViewSerializationModel.cs
@@ -5,12 +5,12 @@ namespace ClearDashboard.Wpf.Application.Models.ProjectSerialization
     /// <summary>
     /// Represents a serialized enhanced view.
     /// </summary>
-    public class SerializedEnhancedView
+    public class EnhancedViewSerializationModel
     {
         public string Title { get; set; } = string.Empty;
         public bool ParatextSync { get; set; }
         public string BBBCCCVVV { get; set; } = "001001001";
-        public List<DisplayOrder> DisplayOrder { get; set; } = new List<DisplayOrder>();
+        public List<EnhancedViewItemMetadatum> EnhancedViewItems { get; set; } = new List<EnhancedViewItemMetadatum>();
         public int VerseOffset { get; set; }
     }
 }
