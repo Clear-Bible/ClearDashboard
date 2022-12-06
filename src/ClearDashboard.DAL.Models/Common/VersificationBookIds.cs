17a2f4c7404f2d640552a8507055c53c2520c7c8
GeoDirk <geodirk@gmail.com>
2022-12-06 10:58:26 -0500

    Updated GetVersificationAndBookIdByParatextProjectId to now include if a book is present and if it has verses or not

--- src/ClearDashboard.DAL.Models/Common/VersificationBookIds.cs
+++ src/ClearDashboard.DAL.Models/Common/VersificationBookIds.cs
@@ -4,7 +4,8 @@ namespace ClearDashboard.DataAccessLayer.Models.Common
 {
     public class VersificationBookIds
     {
-        public ScrVers? Versification;
-        public IEnumerable<string>? BookAbbreviations;
+        public ScrVers? Versification { get; set; }
+        public IEnumerable<string>? BookAbbreviations { get; set; }
+        public List<WorkedOnBook> WorkedOnBooks { get; set; } = new();
     }
 }
