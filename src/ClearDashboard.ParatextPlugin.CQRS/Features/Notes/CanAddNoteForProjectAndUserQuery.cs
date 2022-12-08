16b916a239dc3edbc52905323dcb792907f6bce3
RM <russell.morley@clear.bible>
2022-12-08 11:26:04 -0800

    added check that project is not resource and user is a nonobserver.

---
+++ src/ClearDashboard.ParatextPlugin.CQRS/Features/Notes/CanAddNoteForProjectAndUserQuery.cs
@@ -0,0 +1,11 @@
+﻿using ClearDashboard.DAL.CQRS;
+using MediatR;
+
+namespace ClearDashboard.ParatextPlugin.CQRS.Features.Notes
+{
+    public record CanAddNoteForProjectAndUserQuery(string ParatextUserName, string ParatextProjectId) : IRequest<RequestResult<bool>>
+    {
+        public string ParatextUserName { get; } = ParatextUserName;
+        public string ParatextProjectId { get; } = ParatextProjectId;
+    }
+}
