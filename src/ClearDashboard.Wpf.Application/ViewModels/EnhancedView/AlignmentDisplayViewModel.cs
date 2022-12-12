9582fb4f0ec5532541e2490dacc14cd36ed7fc16
Andy Gray <andy.gray@blackcrater.com>
2022-12-12 18:47:46 +0000

    Added Interlinear and Alignment DisplayViewModels.

---
+++ src/ClearDashboard.Wpf.Application/ViewModels/EnhancedView/AlignmentDisplayViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Caliburn.Micro;
+using ClearDashboard.Wpf.Application.Services;
+using Microsoft.Extensions.Logging;
+using ClearBible.Engine.Corpora;
+using ClearBible.Engine.Tokenization;
+using ClearDashboard.DAL.Alignment.Translation;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ClearDashboard.Wpf.Application.ViewModels.EnhancedView
+{
+    internal class AlignmentDisplayViewModel : VerseDisplayViewModel
+    {
+        private readonly EngineParallelTextRow _parallelTextRow;
+        private readonly AlignmentSet _alignmentSet;
+
+        public override async Task InitializeAsync()
+        {
+            Alignments = await _alignmentSet.GetAlignments(new List<EngineParallelTextRow>() { _parallelTextRow });
+
+            await base.InitializeAsync();
+        }
+
+        public AlignmentDisplayViewModel(EngineParallelTextRow parallelTextRow,
+                                      EngineStringDetokenizer sourceDetokenizer,
+                                      bool isSourceRtl,
+                                      EngineStringDetokenizer targetDetokenizer,
+                                      bool isTargetRtl,
+                                      AlignmentSet alignmentSet,
+                                      NoteManager noteManager, 
+                                      IEventAggregator eventAggregator, 
+                                      ILogger<VerseDisplayViewModel>? logger)
+            : base(noteManager, eventAggregator, logger)
+        {
+            _parallelTextRow = parallelTextRow;
+            if (parallelTextRow.SourceTokens != null)
+            {
+                SourceTokenMap = new TokenMap(parallelTextRow.SourceTokens, sourceDetokenizer, isSourceRtl);
+            }
+            if (parallelTextRow.TargetTokens != null)
+            {
+                TargetTokenMap = new TokenMap(parallelTextRow.TargetTokens, targetDetokenizer, isTargetRtl);
+            }
+
+            _alignmentSet = alignmentSet;
+        }
+    }
+}
---
+++ src/ClearDashboard.Wpf.Application/ViewModels/EnhancedView/InterlinearDisplayViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Caliburn.Micro;
+using ClearDashboard.Wpf.Application.Services;
+using Microsoft.Extensions.Logging;
+using ClearBible.Engine.Corpora;
+using ClearBible.Engine.Tokenization;
+using ClearDashboard.DAL.Alignment.Translation;
+using System.Linq;
+
+namespace ClearDashboard.Wpf.Application.ViewModels.EnhancedView
+{
+    internal class InterlinearDisplayViewModel : VerseDisplayViewModel
+    {
+        public override async Task InitializeAsync()
+        {
+            Translations = await GetTranslations(TranslationSet, SourceTokenMap.Tokens.Select(t => t.TokenId));
+
+            await base.InitializeAsync();
+        }
+
+        public InterlinearDisplayViewModel(EngineParallelTextRow parallelTextRow,
+                                      EngineStringDetokenizer sourceDetokenizer,
+                                      bool isSourceRtl,
+                                      TranslationSet translationSet,
+                                      NoteManager noteManager, 
+                                      IEventAggregator eventAggregator, 
+                                      ILogger<VerseDisplayViewModel>? logger)
+            : base(noteManager, eventAggregator, logger)
+        {
+            SourceTokenMap = new TokenMap(parallelTextRow.SourceTokens!, sourceDetokenizer, isSourceRtl);
+            TranslationSet = translationSet;
+        }
+    }
+}
