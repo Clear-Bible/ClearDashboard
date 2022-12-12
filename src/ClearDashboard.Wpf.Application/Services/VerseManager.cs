e9b5071334a09d2dd7cec28ec72bd88029110046
Andy Gray <andy.gray@blackcrater.com>
2022-12-12 12:47:33 +0000

    Created VerseManager

---
+++ src/ClearDashboard.Wpf.Application/Services/VerseManager.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Caliburn.Micro;
+using Microsoft.Extensions.Logging;
+
+namespace ClearDashboard.Wpf.Application.Services
+{
+    public sealed class VerseManager : PropertyChangedBase
+    {
+        private IEventAggregator EventAggregator { get; }
+        private ILogger<NoteManager>? Logger { get; }
+        private IMediator Mediator { get; }
+
+        public VerseManager(IEventAggregator eventAggregator, ILogger<NoteManager>? logger, IMediator mediator)
+        {
+            EventAggregator = eventAggregator;
+            Logger = logger;
+            Mediator = mediator;
+        }
+    }
+}
