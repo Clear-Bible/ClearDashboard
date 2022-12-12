537e9572f5f9061c3b8ceb16ceaf5eb459e9b257
Andy Gray <andy.gray@blackcrater.com>
2022-12-12 12:47:43 +0000

    Created TranslationManager

---
+++ src/ClearDashboard.Wpf.Application/Services/TranslationManager.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Caliburn.Micro;
+using Microsoft.Extensions.Logging;
+
+namespace ClearDashboard.Wpf.Application.Services
+{
+    public sealed class TranslationManager : PropertyChangedBase
+    {
+        private IEventAggregator EventAggregator { get; }
+        private ILogger<NoteManager>? Logger { get; }
+        private IMediator Mediator { get; }
+
+        public TranslationManager(IEventAggregator eventAggregator, ILogger<NoteManager>? logger, IMediator mediator)
+        {
+            EventAggregator = eventAggregator;
+            Logger = logger;
+            Mediator = mediator;
+        }
+    }
+}
