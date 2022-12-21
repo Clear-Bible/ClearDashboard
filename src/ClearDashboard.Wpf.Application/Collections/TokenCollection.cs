ca815bc3420ae6a0d44713bba2bc97dbd37e29c7
Andy Gray <andy.gray@blackcrater.com>
2022-12-21 15:24:00 +0000

    Added IsFirst() function.

--- src/ClearDashboard.Wpf.Application/Collections/TokenCollection.cs
+++ src/ClearDashboard.Wpf.Application/Collections/TokenCollection.cs
@@ -16,6 +16,26 @@ namespace ClearDashboard.Wpf.Application.Collections
         public IEnumerable<TokenId> TokenIds => this.Select(t => t.TokenId);
 
         /// <summary>
+        /// Determines whether the specified <see cref="TokenId"/> is the first in the collection.
+        /// </summary>
+        /// <param name="tokenId">The token ID to check.</param>
+        /// <returns>True if the <see cref="tokenId"/> is the first in the collection; false otherwise.</returns>
+        public bool IsFirst(TokenId tokenId)
+        {
+            return Count > 0 && Items[0].TokenId.IdEquals(tokenId);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="Token"/> is the first in the collection.
+        /// </summary>
+        /// <param name="token">The token to check.</param>
+        /// <returns>True if the <see cref="token"/> is the first in the collection; false otherwise.</returns>
+        public bool IsFirst(Token token)
+        {
+            return IsFirst(token.TokenId);
+        }
+
+        /// <summary>
         /// Initializes a new instance of the collection.
         /// </summary>
         public TokenCollection()
