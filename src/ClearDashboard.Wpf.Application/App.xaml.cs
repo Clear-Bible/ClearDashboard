8b940b738156d6d6e57aee1defed37f9a1a1e6a6
GeoDirk <geodirk@gmail.com>
2023-01-06 16:42:44 -0500

    fix for CEFSharp not having cache permissions

--- src/ClearDashboard.Wpf.Application/App.xaml.cs
+++ src/ClearDashboard.Wpf.Application/App.xaml.cs
@@ -2,7 +2,10 @@
 
 using System.Diagnostics;
 using System;
+using System.IO;
 using System.Windows.Interop;
+using CefSharp.Wpf;
+using CefSharp;
 
 namespace ClearDashboard.Wpf.Application
 {
@@ -40,6 +43,34 @@ namespace ClearDashboard.Wpf.Application
         {
             // ReSharper disable once ObjectCreationAsStatement
             //new BindingErrorListener(msg => Debugger.Break());
+
+
+            var settings = new CefSettings()
+            {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+            };
+
+            //Example of setting a command line argument
+            //Enables WebRTC
+            // - CEF Doesn't currently support permissions on a per browser basis see https://bitbucket.org/chromiumembedded/cef/issues/2582/allow-run-time-handling-of-media-access
+            // - CEF Doesn't currently support displaying a UI for media access permissions
+            //
+            //NOTE: WebRTC Device Id's aren't persisted as they are in Chrome see https://bitbucket.org/chromiumembedded/cef/issues/2064/persist-webrtc-deviceids-across-restart
+            settings.CefCommandLineArgs.Add("enable-media-stream");
+            // https://peter.sh/experiments/chromium-command-line-switches/#use-fake-ui-for-media-stream
+            settings.CefCommandLineArgs.Add("use-fake-ui-for-media-stream");
+            //For screen sharing add (see https://bitbucket.org/chromiumembedded/cef/issues/2582/allow-run-time-handling-of-media-access#comment-58677180)
+            settings.CefCommandLineArgs.Add("enable-usermedia-screen-capturing");
+
+            //Example of checking if a call to Cef.Initialize has already been made, we require this for
+            //our .Net 5.0 Single File Publish example, you don't typically need to perform this check
+            //if you call Cef.Initialze within your WPF App constructor.
+            if (!Cef.IsInitialized)
+            {
+                //Perform dependency check to make sure all relevant resources are in our output directory.
+                Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+            }
         }
         
     }
