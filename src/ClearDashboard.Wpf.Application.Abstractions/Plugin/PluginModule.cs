6da21dc85f744f6a6e7d35afae97f0bfa8b16758
Michael Gerfen <gerfen@hotmail.com>
2023-01-23 10:12:41 -0800

    Code clean up

--- src/ClearDashboard.Wpf.Application.Abstractions/Plugin/PluginModule.cs
+++ src/ClearDashboard.Wpf.Application.Abstractions/Plugin/PluginModule.cs
@@ -4,12 +4,10 @@ namespace ClearDashboard.Wpf.Application.Plugin
 {
     public abstract class PluginModule : Module
     {
-
         protected override void Load(ContainerBuilder builder)
         {
             RegisterJsonDiscriminatorRegistrar(builder);
             RegisterPluginAbstractions(builder);
-
         }
 
         protected abstract void RegisterJsonDiscriminatorRegistrar(ContainerBuilder builder);
