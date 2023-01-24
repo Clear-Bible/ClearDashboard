c9e9c46c14f2ed1fcbd08e7bd9e22f61a7851ced
Michael Gerfen <gerfen@hotmail.com>
2023-01-23 17:46:02 -0800

    Removed deprecated LocalizationStrings abstract class - now injecting ILocalizationService.

---
+++ src/ClearDashboard.Wpf.Application/Services/LocalizationService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using ClearDashboard.Wpf.Application.Strings;
+using Microsoft.Extensions.Logging;
+
+namespace ClearDashboard.Wpf.Application.Services;
+
+public class LocalizationService : ILocalizationService
+{
+    private readonly ILogger<LocalizationService> _logger;
+
+    public LocalizationService(ILogger<LocalizationService> logger)
+    {
+        _logger = logger;
+    }
+    public string Get(string key)
+    {
+
+        //var resourceManager = new ResourceManager(typeof("ClearDashboard.Aqua.Module.Strings.Resources"))
+        //var resourceSet = resourceManager.GetResourceSet(CultureInfo.GetCultureInfo("en-us"), true, true);
+        //if (resourceSet == null)
+        //    throw new Exception("Unable to create ResourceSet.");
+
+        //var s = resourceSet.GetString("TestKey");
+
+
+        string localizedString;
+        try
+        {
+            localizedString = Resources.ResourceManager.GetString(key, Thread.CurrentThread.CurrentUICulture);
+        }
+        catch (Exception e)
+        {
+            _logger.LogCritical($"Localization string missing for key '{key}' {e.Message} {Thread.CurrentThread.CurrentUICulture.Name}");
+            localizedString = key;
+        }
+
+        if (localizedString == null)
+        {
+            _logger.LogCritical($"Localization string missing for key '{key}' {Thread.CurrentThread.CurrentUICulture.Name}");
+            localizedString = key;
+        }
+        return localizedString;
+    }
+}
