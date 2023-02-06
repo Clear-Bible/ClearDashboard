4dd7baa3c9396cd34d441de750b3ab138abecf80
Michael Gerfen <gerfen@hotmail.com>
2023-02-06 15:47:44 -0800

    Moved form ClearDashboard.Wpf.Application to ClearDashboard.Wpf.Application.Abstractions.

---
+++ src/ClearDashboard.Wpf.Application.Abstractions/Helpers/BindableTextBlock.cs
@@ -0,0 +1,44 @@
+﻿using ClearDashboard.DataAccessLayer;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace ClearDashboard.Wpf.Application.Helpers
+{
+    public class BindableTextBlock : TextBlock
+    {
+        public ObservableCollection<Inline> InlineList
+        {
+            get => (ObservableCollection<Inline>)GetValue(InlineListProperty);
+            set => SetValue(InlineListProperty, value);
+        }
+
+        private static ObservableCollection<Inline> _oldValue;
+        
+        public new string FontFamily = FontNames.DefaultFontFamily;
+        public new float FontSize = 13;
+
+        public new static readonly DependencyProperty FontSizeProperty =
+            DependencyProperty.Register("FontSize", typeof(float), typeof(BindableTextBlock), new UIPropertyMetadata(13f, OnPropertyChanged));
+        
+        public new static readonly DependencyProperty FontFamilyProperty =
+            DependencyProperty.Register("FontFamily", typeof(string), typeof(BindableTextBlock), new UIPropertyMetadata(FontNames.DefaultFontFamily, OnPropertyChanged));
+        
+        public static readonly DependencyProperty InlineListProperty =
+            DependencyProperty.Register("InlineList", typeof(ObservableCollection<Inline>), typeof(BindableTextBlock), new UIPropertyMetadata(null, OnPropertyChanged));
+
+        private static void OnPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var textBlock = (BindableTextBlock)sender;
+            textBlock.Inlines.Clear();
+            if (e.NewValue != null  && (ObservableCollection<Inline>)e.NewValue != _oldValue)
+            {
+                textBlock.Inlines.AddRange((ObservableCollection<Inline>)e.NewValue);
+            }
+            _oldValue = (ObservableCollection<Inline>)e.NewValue;
+        }
+
+
+    }
+}
