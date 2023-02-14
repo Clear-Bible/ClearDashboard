422d6a83b2a925ff03828cdc28a04534b9575931
morleycb <chris@morleyconsulting.net>
2023-02-14 10:06:45 -0800

    For manual alignments:  added AlignmentSet.DeleteAlignment and changed AlignmentSet.PutAlignment to soft delete alignments instead of removing records from the database.

---
+++ src/ClearDashboard.DAL.Alignment/Features/Translation/DeleteAlignmentByAlignmentIdCommand.cs
@@ -0,0 +1,10 @@
+﻿using ClearBible.Engine.Corpora;
+using ClearDashboard.DAL.Alignment.Translation;
+using ClearDashboard.DAL.CQRS.Features;
+using MediatR;
+
+namespace ClearDashboard.DAL.Alignment.Features.Translation
+{
+    public record DeleteAlignmentByAlignmentIdCommand(
+        AlignmentId AlignmentId) : ProjectRequestCommand<Unit>;
+}
