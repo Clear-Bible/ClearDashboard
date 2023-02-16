7eaba816699a5a6c6099ed9dabe8b4644fd12e59
GeoDirk <geodirk@gmail.com>
2023-02-16 09:55:52 -0500

    staging views/viewmodels

---
+++ src/ClearDashboard.Wpf.Application.Abstractions/Services/SystemPowerModes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearDashboard.Wpf.Application.Services
+{
+    public class SystemPowerModes
+    {
+        public bool IsLaptop => CheckIfLaptop();
+
+        private bool CheckIfLaptop()
+        {
+            return false;
+        }
+
+        public async Task TurnOnHighPerformanceMode()
+        {
+
+        }
+
+        public async Task TurnOffHighPerformanceMode()
+        {
+
+        }
+
+    }
+}
---
+++ src/ClearDashboard.Wpf.Application/ViewModels/DashboardSettings/DashboardSettingsViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Caliburn.Micro;
+
+namespace ClearDashboard.Wpf.Application.ViewModels.DashboardSettings
+{
+    public class DashboardSettingsViewModel : Screen
+    {
+
+        #region Member Variables   
+
+        #endregion //Member Variables
+
+
+        #region Public Properties
+
+        #endregion //Public Properties
+
+
+        #region Observable Properties
+
+        private bool _isPowerModesEnabled;
+        public bool IsPowerModesEnabled
+        {
+            get => _isPowerModesEnabled;
+            set
+            {
+                _isPowerModesEnabled = value; 
+                NotifyOfPropertyChange(() => IsPowerModesEnabled);
+            }
+        }
+
+
+        // controls the group box IsEnabled
+        private bool _isPowerModesBoxEnabled;
+        public bool IsPowerModesBoxEnabled
+        {
+            get => _isPowerModesBoxEnabled;
+            set
+            {
+                _isPowerModesBoxEnabled = value; 
+                NotifyOfPropertyChange(() => IsPowerModesBoxEnabled);
+            }
+        }
+
+
+
+        #endregion //Observable Properties
+
+
+        #region Constructor
+
+        // ReSharper disable once EmptyConstructor
+        public DashboardSettingsViewModel()
+        {
+            // for Caliburn Micro
+        }
+
+        protected override void OnViewReady(object view)
+        {
+
+
+            base.OnViewReady(view);
+        }
+
+        #endregion //Constructor
+
+
+        #region Methods
+
+        #endregion // Methods
+
+    }
+}
