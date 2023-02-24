682f4f637018f5a94acd0d9c92c32f57cd50989e
Andy Gray <andy.gray@blackcrater.com>
2023-02-23 19:40:24 -0800

    EventArgs for Lexicon

---
+++ src/ClearDashboard.Wpf.Application.Abstractions/Events/LexemeFormEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using ClearDashboard.DAL.Alignment.Lexicon;
+using ClearDashboard.Wpf.Application.ViewModels.EnhancedView.Lexicon;
+
+namespace ClearDashboard.Wpf.Application.Events
+{
+    public class LexemeFormEventArgs : RoutedEventArgs
+    {
+        public Form Form { get; set; } = new();
+        public LexemeViewModel Lexeme { get; set; } = new();
+    }
+}
---
+++ src/ClearDashboard.Wpf.Application.Abstractions/Events/MeaningEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using ClearDashboard.DAL.Alignment.Lexicon;
+using ClearDashboard.Wpf.Application.ViewModels.EnhancedView.Lexicon;
+
+namespace ClearDashboard.Wpf.Application.Events
+{
+    public class MeaningEventArgs : RoutedEventArgs
+    {
+        public MeaningViewModel Meaning { get; set; } = new();
+        public LexemeViewModel Lexeme { get; set; } = new();
+    }
+}
