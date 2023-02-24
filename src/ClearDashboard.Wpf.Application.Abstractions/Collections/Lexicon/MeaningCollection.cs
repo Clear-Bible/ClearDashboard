2004b5d8cebac583d60034e71819083cd5363b8f
Andy Gray <andy.gray@blackcrater.com>
2023-02-23 19:40:09 -0800

    Collections for lexicon

---
+++ src/ClearDashboard.Wpf.Application.Abstractions/Collections/Lexicon/MeaningCollection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Caliburn.Micro;
+using ClearDashboard.DAL.Alignment.Lexicon;
+
+namespace ClearDashboard.Wpf.Application.Collections.Lexicon
+{
+    public class MeaningCollection : BindableCollection<Meaning>
+    {
+        public MeaningCollection()
+        {
+        }
+
+        public MeaningCollection(IEnumerable<Meaning> meanings) : base(meanings)
+        {
+        }
+    }
+}
---
+++ src/ClearDashboard.Wpf.Application.Abstractions/Collections/Lexicon/TranslationViewModelCollection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Caliburn.Micro;
+using ClearDashboard.DAL.Alignment.Lexicon;
+using ClearDashboard.Wpf.Application.ViewModels.EnhancedView.Lexicon;
+
+namespace ClearDashboard.Wpf.Application.Collections.Lexicon
+{
+    /// <summary>
+    /// A bindable collection of <see cref="Translation"/> instances.
+    /// </summary>
+    public sealed class TranslationViewModelCollection : BindableCollection<TranslationViewModel>
+    {
+        /// <summary>
+        /// Initializes a new instance of the collection.
+        /// </summary>
+        public TranslationViewModelCollection()
+        {
+        }
+
+        public TranslationViewModelCollection(IEnumerable<Translation> translations)
+        {
+            AddRange(translations.Select(t => new TranslationViewModel(t)));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the collection, initializing it with a sequence of translations.
+        /// </summary>
+        /// <param name="collection">The collection from which the elements are copied.</param>
+        public TranslationViewModelCollection(IEnumerable<TranslationViewModel> collection) : base(collection)
+        {
+        }
+    }
+}
