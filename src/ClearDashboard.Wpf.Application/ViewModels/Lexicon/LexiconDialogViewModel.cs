41c8f9aba50b75d7d44de9fda4cf074378509972
Andy Gray <andy.gray@blackcrater.com>
2023-02-27 23:45:41 +0100

    Updated saving of translation.  Null protection for properties.

--- src/ClearDashboard.Wpf.Application/ViewModels/Lexicon/LexiconDialogViewModel.cs
+++ src/ClearDashboard.Wpf.Application/ViewModels/Lexicon/LexiconDialogViewModel.cs
@@ -1,7 +1,6 @@
-﻿#define DEMO
+﻿//#define DEMO
 
 using System;
-using System.Collections.Generic;
 using System.Dynamic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,14 +8,15 @@ using System.Windows;
 using System.Windows.Controls.Primitives;
 using Autofac;
 using Caliburn.Micro;
+using ClearDashboard.DAL.Alignment.Translation;
 using ClearDashboard.Wpf.Application.Collections.Lexicon;
 using ClearDashboard.Wpf.Application.Events.Lexicon;
 using ClearDashboard.Wpf.Application.Infrastructure;
+using ClearDashboard.Wpf.Application.Messages.Lexicon;
 using ClearDashboard.Wpf.Application.Services;
 using ClearDashboard.Wpf.Application.UserControls.Lexicon;
 using ClearDashboard.Wpf.Application.ViewModels.EnhancedView;
 using ClearDashboard.Wpf.Application.ViewModels.EnhancedView.Lexicon;
-using ClearDashboard.Wpf.Application.Views.Lexicon;
 using MediatR;
 using Microsoft.Extensions.Logging;
 // ReSharper disable UnusedMember.Global
@@ -26,14 +26,38 @@ namespace ClearDashboard.Wpf.Application.ViewModels.Lexicon
     public class LexiconDialogViewModel : DashboardApplicationScreen
     {
         private readonly LexiconManager? _lexiconManager;
-        // ReSharper disable once ConvertToAutoProperty
-        private LexiconManager LexiconManager => _lexiconManager!;
+        private LexiconManager LexiconManager
+        {
+            get
+            {
+                if (_lexiconManager == null) throw new Exception("Cannot perform operation as LexiconManager is null");
+                return _lexiconManager!;
+            }
+        }
 
-        public string DialogTitle => $"Translation/Lexeme: {TokenDisplay?.TranslationSurfaceText}";
+        private TokenDisplayViewModel? _tokenDisplay;
+        public TokenDisplayViewModel TokenDisplay
+        {
+            get
+            {
+                if (_tokenDisplay == null) throw new Exception("Cannot perform operation as TokenDisplay is null");
+                return _tokenDisplay;
+            }
+            set => _tokenDisplay = value;
+        }
 
-        public TokenDisplayViewModel? TokenDisplay { get; set; }
+        private InterlinearDisplayViewModel? _interlinearDisplayViewModel;
+        public InterlinearDisplayViewModel InterlinearDisplay
+        {
+            get
+            {
+                if (_interlinearDisplayViewModel == null) throw new Exception("Cannot perform operation as InterlinearDisplay is null");
+                return _interlinearDisplayViewModel!;
+            }
+            set => _interlinearDisplayViewModel = value;
+        }
 
-        public InterlinearDisplayViewModel? InterlinearDisplay { get; set; }
+        public string DialogTitle => $"Translation/Lexeme: {TokenDisplay.TranslationSurfaceText}";
 
         private LexemeViewModel? _lexeme;
         public LexemeViewModel? Lexeme
@@ -56,12 +80,7 @@ namespace ClearDashboard.Wpf.Application.ViewModels.Lexicon
             set => Set(ref _concordance, value);
         }
 
-        private IEnumerable<TranslationOption> _translationOptions = new List<TranslationOption>();
-        public IEnumerable<TranslationOption> TranslationOptions
-        {
-            get => _translationOptions;
-            private set => Set(ref _translationOptions, value);
-        }
+        public bool ApplyToAll { get; set; } = true;
 
         private Visibility _progressBarVisibility = Visibility.Visible;
         public Visibility ProgressBarVisibility
@@ -70,30 +89,11 @@ namespace ClearDashboard.Wpf.Application.ViewModels.Lexicon
             private set => Set(ref _progressBarVisibility, value);
         }
 
-        private async Task OnLoadedAsync()
+        private bool _applyEnabled = false;
+        public bool ApplyEnabled
         {
-            //if (TokenDisplay.Translation != null && !TokenDisplay.Translation.IsDefault)
-            //{
-            //    TranslationOptions = await InterlinearDisplay.GetTranslationOptionsAsync(TokenDisplay.Token);
-            //    CurrentTranslationOption = TranslationOptions.FirstOrDefault(to => to.Word == TokenDisplay.TargetTranslationText);
-
-            //    OnUIThread(() =>
-            //    {
-            //        if (CurrentTranslationOption == null)
-            //        {
-            //            TranslationSelectorControl.TranslationValue.Text = TokenDisplay.TargetTranslationText;
-            //            TranslationSelectorControl.TranslationValue.SelectAll();
-            //        }
-            //        TranslationSelectorControl.TranslationOptionsVisibility = Visibility.Visible;
-            //    });
-            //}
-            //OnUIThread(() =>
-            //{
-            //    TranslationSelectorControl.TranslationControlsVisibility = Visibility.Visible;
-            //    ProgressBarVisibility = Visibility.Collapsed;
-            //    TranslationSelectorControl.TranslationValue.SelectAll();
-            //    TranslationSelectorControl.TranslationValue.Focus();
-            //});
+            get => _applyEnabled;
+            private set => Set(ref _applyEnabled, value);
         }
 
         public async void ApplyTranslation()
@@ -104,12 +104,15 @@ namespace ClearDashboard.Wpf.Application.ViewModels.Lexicon
 
                 async Task SaveTranslation()
                 {
-                    //await InterlinearDisplay.PutTranslationAsync(e.Translation, e.TranslationActionType);
+                    if (SelectedTranslation != null && !string.IsNullOrWhiteSpace(SelectedTranslation.Text))
+                    {
+                        await InterlinearDisplay.PutTranslationAsync(new Translation(TokenDisplay.TokenForTranslation, SelectedTranslation.Text, Translation.OriginatedFromValues.Assigned),
+                                                                     ApplyToAll ? TranslationActionTypes.PutPropagate : TranslationActionTypes.PutNoPropagate);
+                    }
                 }
-
+#if !DEMO
                 await System.Windows.Application.Current.Dispatcher.InvokeAsync(SaveTranslation);
-
-                //System.Windows.Application.Current.Dispatcher.Invoke(() => DialogResult = true);
+#endif
             }
             catch (Exception ex)
             {
@@ -135,6 +138,7 @@ namespace ClearDashboard.Wpf.Application.ViewModels.Lexicon
                 if (!string.IsNullOrWhiteSpace(e.Lexeme.Lemma))
                 {
                     Lexeme = await LexiconManager.CreateLexemeAsync(e.Lexeme.Lemma, e.Lexeme.Language, e.Lexeme.Type);
+                    await EventAggregator.PublishOnUIThreadAsync(new LexemeAddedMessage(Lexeme));
                 }
             }
             else
@@ -142,6 +146,19 @@ namespace ClearDashboard.Wpf.Application.ViewModels.Lexicon
                 Logger?.LogError($"Cannot create new lexeme because {e.Lexeme.Lemma} already has a LexemeId.");
             }
 #endif
+        }
+        public async void OnLexemeDeleted(object sender, LexemeEventArgs e)
+        {
+#if !DEMO
+            if (e.Lexeme.LexemeId != null)
+            {
+                await LexiconManager.DeleteLexemeAsync(e.Lexeme);
+            }
+            else
+            {
+                Logger?.LogError($"Cannot delete lexeme because {e.Lexeme.Lemma} does not have a LexemeId.");
+            }
+#endif 
         }
 
         public async void OnLexemeFormAdded(object sender, LexemeFormEventArgs e)
@@ -176,7 +193,6 @@ namespace ClearDashboard.Wpf.Application.ViewModels.Lexicon
         {
 #if !DEMO
 #endif
-
         }
 
         public async void OnSemanticDomainAdded(object sender, SemanticDomainEventArgs e)
@@ -215,23 +231,46 @@ namespace ClearDashboard.Wpf.Application.ViewModels.Lexicon
             await EventAggregator.PublishOnUIThreadAsync(new LexiconTranslationMovedMessage(e.Translation, e.Meaning));
 #if !DEMO
 #endif
-
         }
 
         private LexiconTranslationViewModel? SelectedTranslation { get; set; }
-        public async void OnTranslationSelected(object sender, LexiconTranslationEventArgs e)
+        public void OnTranslationSelected(object sender, LexiconTranslationEventArgs e)
         {
             SelectedTranslation = e.Translation;
+            ApplyEnabled = !string.IsNullOrWhiteSpace(SelectedTranslation.Text);
+        }
+        
+        public void OnTranslationAdded(object sender, LexiconTranslationEventArgs e)
+        {
+            SelectedTranslation = e.Translation;
+            ApplyEnabled = !string.IsNullOrWhiteSpace(SelectedTranslation.Text);
+        }
+
+        public void OnNewTranslationChanged(object sender, LexiconTranslationEventArgs e)
+        {
+            SelectedTranslation = e.Translation;
+            ApplyEnabled = !string.IsNullOrWhiteSpace(SelectedTranslation.Text);
+        }
+
+        private async Task BuildConcordance()
+        {
+            var translationOptions = await InterlinearDisplay.GetTranslationOptionsAsync(TokenDisplay.Token);
+            foreach (var translationOption in translationOptions)
+            {
+                if (Lexeme == null || !Lexeme.ContainsTranslationText(translationOption.Word))
+                {
+                    Concordance.Add(new LexiconTranslationViewModel(translationOption.Word, Convert.ToInt32(translationOption.Count)));
+                }
+            }
         }
 
         protected override async Task OnInitializeAsync(CancellationToken cancellationToken)
         {
             await base.OnInitializeAsync(cancellationToken);
 
-            if (Lexeme == null && TokenDisplay != null)
-            {
-                Lexeme = await LexiconManager!.GetLexemeAsync(TokenDisplay.TranslationSurfaceText);
-            }
+            Lexeme ??= await LexiconManager!.GetLexemeAsync(TokenDisplay.TranslationSurfaceText);
+            await BuildConcordance();
+
             OnUIThread(() => ProgressBarVisibility = Visibility.Collapsed);
         }
 
@@ -268,6 +307,7 @@ namespace ClearDashboard.Wpf.Application.ViewModels.Lexicon
         {
             _lexiconManager = lexiconManager;
 
+            LexemeEditor.EventAggregator = eventAggregator;
             ConcordanceDisplay.EventAggregator = eventAggregator;
             MeaningEditor.EventAggregator = eventAggregator;
         }
