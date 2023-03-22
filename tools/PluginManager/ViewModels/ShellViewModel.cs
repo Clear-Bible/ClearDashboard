93a8a3e0611c015d799613f499dd5fafa94443b2
GeoDirk <geodirk@gmail.com>
2023-03-22 10:15:53 -0400

    adding plugin manager

---
+++ tools/PluginManager/ViewModels/ShellViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Caliburn.Micro;
+using System.Reflection;
+
+namespace PluginManager.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        #region Member Variables   
+
+        
+        #endregion //Member Variables
+
+
+
+        #region Public Properties
+
+        
+        #endregion //Public Properties
+
+
+        #region Observable Properties
+
+        private string? _version;
+        public string? Version
+        {
+            get => _version;
+            set => Set(ref _version, value);
+        }
+
+
+        
+        #endregion //Observable Properties
+
+
+        #region Constructor
+
+        public ShellViewModel()
+        {
+
+        }
+
+        protected override async void OnViewLoaded(object view)
+        {
+            //get the assembly version
+            var thisVersion = Assembly.GetEntryAssembly().GetName().Version;
+            Version = $"Clear Dashboard Plugin Manager  -  Version: {thisVersion.Major}.{thisVersion.Minor}.{thisVersion.Build}.{thisVersion.Revision}";
+        }
+        
+        #endregion //Constructor
+
+
+
+        #region Methods
+
+        
+
+        #endregion // Methods
+
+
+    }
+}
