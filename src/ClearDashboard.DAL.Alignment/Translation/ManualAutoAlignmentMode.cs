8b2d64bde24f054a5e6bdf25fb2a7dbf6eb1f9e8
morleycb <chris@morleyconsulting.net>
2023-03-21 17:20:20 -0700

    Implemented ManualAutoAlignmentModes

---
+++ src/ClearDashboard.DAL.Alignment/Translation/ManualAutoAlignmentMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearDashboard.DAL.Alignment.Translation
+{
+    public enum ManualAutoAlignmentMode
+    {
+        All,
+        ManualOnly,
+        ManualAndOnlyNonManualAuto
+    }
+}
