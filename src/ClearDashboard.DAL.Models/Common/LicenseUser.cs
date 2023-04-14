dbf46f73cfad4d54cbd5a9d4cd88641ee207dd67
Robertson Brinker <robertsonbrinker@gmail.com>
2023-04-14 10:39:18 -0400

    added temporary LicenseUser class to convert faulty Ids to genuine Guids

--- src/ClearDashboard.DAL.Models/Common/LicenseUser.cs
+++ src/ClearDashboard.DAL.Models/Common/LicenseUser.cs
@@ -15,6 +15,17 @@ namespace ClearDashboard.DataAccessLayer.Models
         public LicenseUserMatchType MatchType { get; set; }
     }
 
+    public class TemporaryLicenseUser 
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? LicenseKey { get; set; }
+        public string FullName => $"{FirstName}, {LastName}";
+        public string ParatextUserName { get; set; } = null;
+        public LicenseUserMatchType MatchType { get; set; }
+        public string Id { get; set; }
+    }
+
     public enum LicenseUserMatchType
     {
         Match,
