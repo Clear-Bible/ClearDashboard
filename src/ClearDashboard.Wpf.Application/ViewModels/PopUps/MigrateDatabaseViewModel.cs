1315191467b7655e461ea7dd2351227aeccedfa4
GeoDirk <geodirk@gmail.com>
2023-05-02 22:22:03 -0400

    visibility/xaml cues

--- src/ClearDashboard.Wpf.Application/ViewModels/PopUps/MigrateDatabaseViewModel.cs
+++ src/ClearDashboard.Wpf.Application/ViewModels/PopUps/MigrateDatabaseViewModel.cs
@@ -11,6 +11,13 @@ using ClearDashboard.DAL.Alignment.Corpora;
 using ClearDashboard.DataAccessLayer.Models;
 using ParallelCorpus = ClearDashboard.DAL.Alignment.Corpora.ParallelCorpus;
 using ClearDashboard.DataAccessLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using ClearDashboard.Wpf.Application.Helpers;
 
 namespace ClearDashboard.Wpf.Application.ViewModels.PopUps
 {
@@ -27,6 +34,8 @@ namespace ClearDashboard.Wpf.Application.ViewModels.PopUps
         private TopLevelProjectIds _topLevelProjectIds;
         private ProjectDbContext _projectDbContext;
 
+        private bool _completedRuns = false;
+
         #endregion //Member Variables
 
 
@@ -38,6 +47,53 @@ namespace ClearDashboard.Wpf.Application.ViewModels.PopUps
 
 
         #region Observable Properties
+
+        private ObservableCollection<ParallelIdList> _parallelIdLists = new();
+        public ObservableCollection<ParallelIdList> ParallelIdLists
+        {
+            get => _parallelIdLists;
+            set
+            {
+                _parallelIdLists = value;
+                NotifyOfPropertyChange(() => ParallelIdLists);
+            }
+        }
+
+        private Visibility _startupProgressCircle = Visibility.Visible;
+        public Visibility StartupProgressCircle
+        {
+            get => _startupProgressCircle;
+            set
+            {
+                _startupProgressCircle = value; 
+                NotifyOfPropertyChange(nameof(StartupProgressCircle));
+            }
+        }
+
+
+        private bool _startEnabled = true;
+        public bool StartEnabled
+        {
+            get => _startEnabled;
+            set
+            {
+                _startEnabled = value;
+                NotifyOfPropertyChange(() => StartEnabled);
+            }
+        }
+
+        private bool _closeEnabled = true;
+        public bool CloseEnabled
+        {
+            get => _closeEnabled;
+            set
+            {
+                _closeEnabled = value;
+                NotifyOfPropertyChange(() => CloseEnabled);
+            }
+        }
+
+
 
         #endregion //Observable Properties
 
@@ -65,7 +121,13 @@ namespace ClearDashboard.Wpf.Application.ViewModels.PopUps
             _localizationService = localizationService;
         }
 
-        protected override async void OnViewReady(object view)
+        protected override void OnViewReady(object view)
+        {
+            base.OnViewReady(view);
+        }
+
+
+        protected override async void OnViewLoaded(object view)
         {
             ProjectManager!.CurrentDashboardProject = Project;
 
@@ -75,7 +137,22 @@ namespace ClearDashboard.Wpf.Application.ViewModels.PopUps
 
             var parallelCorpusIds = _topLevelProjectIds.ParallelCorpusIds;
 
-            base.OnViewReady(view);
+            ParallelIdLists = new();
+
+            foreach (var parallelCorpusId in parallelCorpusIds)
+            {
+                ParallelIdLists.Add(new ParallelIdList
+                {
+                    Status = ParallelIdList.JobStatus.NeedToRun,
+                    ParallelCorpusId = parallelCorpusId,
+                });
+            }
+
+            StartupProgressCircle = Visibility.Collapsed;
+
+            _completedRuns = true;
+
+            base.OnViewLoaded(view);
         }
 
         #endregion //Constructor
@@ -95,7 +172,91 @@ namespace ClearDashboard.Wpf.Application.ViewModels.PopUps
             await parallelCorpus.Update(_mediator);
         }
 
+        public async void Start()
+        {
+            StartEnabled = false;
+            CloseEnabled = false;
+
+            foreach (var parallelId in ParallelIdLists)
+            {
+                parallelId.Status = ParallelIdList.JobStatus.Working;
+                NotifyOfPropertyChange(nameof(ParallelIdLists));
+                await Task.Delay(500);
+
+                var parallelCorpus = await ParallelCorpus.Get(_mediator, parallelId.ParallelCorpusId);
+                await RunRegenerationOnParallelCorpus(parallelCorpus);
+
+                parallelId.Status = ParallelIdList.JobStatus.Completed;
+                NotifyOfPropertyChange(nameof(ParallelIdLists));
+                await Task.Delay(500);
+            }
+
+            CloseEnabled = true;
+
+            PlaySound.PlaySoundFromResource(SoundType.Success);
+        }
+
+        public void Close()
+        {
+            if (_completedRuns)
+            {
+                
+            }
+
+            this.TryCloseAsync();
+        }
 
         #endregion // Methods
+    }
+
+    public class ParallelIdList : INotifyPropertyChanged
+    {
+        public enum JobStatus
+        {
+            NeedToRun,
+            Working,
+            Completed
+        }
+
+        private JobStatus _status;
+        public JobStatus Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                OnPropertyChanged($"{nameof(Status)}");
+            }
+        }
+
+
+        private ParallelCorpusId parallelCorpusId;
+        public ParallelCorpusId ParallelCorpusId
+        {
+            get => parallelCorpusId;
+            set
+            {
+                parallelCorpusId = value; 
+                OnPropertyChanged($"{nameof(ParallelCorpusId)}");
+            }
+        }
+
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
