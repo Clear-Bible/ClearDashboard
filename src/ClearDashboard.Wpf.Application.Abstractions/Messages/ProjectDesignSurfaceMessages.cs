52f99d3a2abbe9307bf77688398037f47f209868
GeoDirk <geodirk@gmail.com>
2023-05-04 15:18:50 -0400

    mostly working

--- src/ClearDashboard.Wpf.Application.Abstractions/Messages/ProjectDesignSurfaceMessages.cs
+++ src/ClearDashboard.Wpf.Application.Abstractions/Messages/ProjectDesignSurfaceMessages.cs
@@ -10,11 +10,11 @@ namespace ClearDashboard.Wpf.Application.Messages
     public record NodeSelectedChangedMessage(object? Node);
     public record ConnectionSelectedChangedMessage(Guid ConnectorId);
     public record CorpusAddedMessage(string ParatextId);
-    public record CorpusDeletedMessage(string ParatextId);
+    public record CorpusDeletedMessage(string ParatextId, Guid CorpusId);
     public record CorpusSelectedMessage(string ParatextId);
     public record CorpusDeselectedMessage(string ParatextId);
     public record ParallelCorpusAddedMessage(string SourceParatextId, string TargetParatextId, Guid ConnectorGuid);
-    public record ParallelCorpusDeletedMessage(string SourceParatextId, string TargetParatextId, Guid ConnectorGuid);
+    public record ParallelCorpusDeletedMessage(string SourceParatextId, string TargetParatextId, Guid ConnectorGuid, Guid ParallelCorpusId);
     public record ParallelCorpusSelectedMessage(string SourceParatextId, string TargetParatextId, Guid ConnectorGuid);
     public record ParallelCorpusDeselectedMessage(Guid ConnectorGuid);
 
