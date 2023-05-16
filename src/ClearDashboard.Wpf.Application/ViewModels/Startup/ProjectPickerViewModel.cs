f09bf43123cb2513548d560de6ec5b7216960314
Robertson Brinker <robertsonbrinker@gmail.com>
2023-05-16 13:53:28 -0400

    2 other hot fixes
    1. dashboard name appearing on new project
    2. preventing book selection until usfm is done checking

--- src/ClearDashboard.Wpf.Application/ViewModels/Startup/ProjectPickerViewModel.cs
+++ src/ClearDashboard.Wpf.Application/ViewModels/Startup/ProjectPickerViewModel.cs
@@ -439,7 +439,7 @@ namespace ClearDashboard.Wpf.Application.ViewModels.Startup
             
 
             ProjectManager!.CurrentDashboardProject = project;
-            EventAggregator.PublishOnUIThreadAsync(new DashboardProjectMessage(ProjectManager!.CurrentDashboardProject));
+            EventAggregator.PublishOnUIThreadAsync(new DashboardProjectNameMessage(ProjectManager!.CurrentDashboardProject.ProjectName));
 
             OpenProjectManager.AddProjectToOpenProjectList(ProjectManager);
 
