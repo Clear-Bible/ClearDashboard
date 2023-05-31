2afe164e3b29e4b38241ed3b2bb7cd38a6d7ee7f
Christopher Morley <chris.morley@clear.bible>
2023-05-31 09:45:40 -0700

    tiny

--- src/ClearDashboard.DAL.Alignment/Features/Translation/GetTranslationsByTranslationSetIdAndTokenIdsQuery.cs
+++ src/ClearDashboard.DAL.Alignment/Features/Translation/GetTranslationsByTranslationSetIdAndTokenIdsQuery.cs
@@ -5,8 +5,6 @@ using ClearDashboard.DAL.CQRS.Features;
 namespace ClearDashboard.DAL.Alignment.Features.Translation
 {
     /// <summary>
-    /// FIXME:  perhaps AlignmentOriginationFilterMode used in the non-denormalized alignment
-    /// portion of this query should come from a dashboard setting/configuration
     /// </summary>
     /// <param name="TranslationSetId"></param>
     /// <param name="TokenIds"></param>
