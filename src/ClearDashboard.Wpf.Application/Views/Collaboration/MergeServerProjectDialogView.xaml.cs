d93bdd01fd4d6a9867764539086133e40e955e4c
GeoDirk <geodirk@gmail.com>
2023-06-09 14:05:58 -0400

    Update MergeServerProjectDialogView.xaml.cs

--- src/ClearDashboard.Wpf.Application/Views/Collaboration/MergeServerProjectDialogView.xaml.cs
+++ src/ClearDashboard.Wpf.Application/Views/Collaboration/MergeServerProjectDialogView.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Specialized;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -15,25 +13,11 @@ namespace ClearDashboard.Wpf.Application.Views.Collaboration
         {
             InitializeComponent();
 
-            //((INotifyCollectionChanged)ProgressMessages.ItemsSource).CollectionChanged +=
-            //    new NotifyCollectionChangedEventHandler(ProgressMessagesCollectionChanged);
         }
-
-        //public void ProgressMessagesCollectionChanged(Object sender, NotifyCollectionChangedEventArgs e)
-        //{
-        //    if (ProgressMessages.Items.Count > 0)
-        //    {
-        //        // auto scroll last message into view
-        //        ProgressMessages.ScrollIntoView(ProgressMessages.Items[^1]);
-        //    }
-        //}
 
         private void MergeServerProjectDialogView_OnLoaded(object sender, RoutedEventArgs e)
         {
             ProgressMessages.Focus();
-
-
-
 
             //// listen for changes to the lower listview to make it scroll back 
             //// to the top
