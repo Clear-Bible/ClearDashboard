6fe1a5f5d8faad4a632274344d376c1ecf7c0d3d
Andy Gray <andy.gray@blackcrater.com>
2023-06-12 16:02:43 -0700

    UI polish for Aqua dialog (#670)

---
+++ src/ClearDashboard.Aqua.Module/Converters/BooleanAndVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ClearDashboard.Aqua.Module.Converters
+{
+    public sealed class BooleanAndVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            foreach (object value in values)
+            {
+                if ((value is bool) && (bool)value == false)
+                {
+                    return Visibility.Hidden;
+                }
+            }
+            return Visibility.Visible;
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException("BooleanAndVisibilityConverter is a OneWay converter.");
+        }
+    }
+}
