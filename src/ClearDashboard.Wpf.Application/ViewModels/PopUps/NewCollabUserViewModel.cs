a9483655837e4a8b0270cd62f91f17c863e6bd0c
GeoDirk <geodirk@gmail.com>
2023-06-27 10:10:20 -0400

    Localization of NewCollabUser View/ViewModel

--- src/ClearDashboard.Wpf.Application/ViewModels/PopUps/NewCollabUserViewModel.cs
+++ src/ClearDashboard.Wpf.Application/ViewModels/PopUps/NewCollabUserViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using Caliburn.Micro;
 using ClearDashboard.Collaboration.Services;
+using ClearDashboard.DAL.Alignment.Features.Denormalization;
 using ClearDashboard.DataAccessLayer.Models;
 using ClearDashboard.Wpf.Application.Helpers;
 using ClearDashboard.Wpf.Application.Infrastructure;
@@ -11,6 +12,7 @@ using MailKit.Net.Smtp;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using MimeKit;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
@@ -333,10 +335,10 @@ namespace ClearDashboard.Wpf.Application.ViewModels.PopUps
                 var mailMessage = new MimeMessage();
                 mailMessage.From.Add(new MailboxAddress("cleardas@cleardashboard.org", "cleardas@cleardashboard.org"));
                 mailMessage.To.Add(new MailboxAddress(FirstName + " " + LastName, Email));
-                mailMessage.Subject = "ClearDashboard Email Validation Code";
+                mailMessage.Subject = LocalizationStrings.Get("NewCollabUserView_DashboardEmailValidationCode", _logger); //"ClearDashboard Email Validation Code";
                 mailMessage.Body = new TextPart("plain")
                 {
-                    Text = "Email Verification Code: " + _emailValidationString
+                    Text = LocalizationStrings.Get("NewCollabUserView_EmailValidationCode", _logger) + " " + _emailValidationString //Email Verification Code: 
                 };
 
                 try
@@ -364,7 +366,7 @@ namespace ClearDashboard.Wpf.Application.ViewModels.PopUps
             }
             else
             {
-                ErrorMessage = "User is already on the system!";
+                ErrorMessage = LocalizationStrings.Get("NewCollabUserView_UserOnSystem", _logger); //"User is already on the system!";
             }
 
 
@@ -388,7 +390,7 @@ namespace ClearDashboard.Wpf.Application.ViewModels.PopUps
 
             if (user.Id == 0)
             {
-                ErrorMessage = "Error Creating user on Server";
+                ErrorMessage = LocalizationStrings.Get("NewCollabUserView_ErrorCreatingUser", _logger); //"Error Creating user on Server";
 
                 CollaborationConfig = new();
             }
@@ -417,12 +419,12 @@ namespace ClearDashboard.Wpf.Application.ViewModels.PopUps
 
                 if (results)
                 {
-                    SaveGitLabUserMessage = "Saved to remote server";
+                    SaveGitLabUserMessage = LocalizationStrings.Get("NewCollabUserView_SavedToRemoteServer", _logger); //Saved to remote server
                     SaveMessageForegroundColor = Brushes.Green;
                 }
                 else
                 {
-                    SaveGitLabUserMessage = "User already exists on server";
+                    SaveGitLabUserMessage = LocalizationStrings.Get("NewCollabUserView_UserAlreadyExists", _logger); //User already exists on server
                     SaveMessageForegroundColor = Brushes.Red;
                 }
             }
