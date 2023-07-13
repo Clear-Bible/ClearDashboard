72f8702fe7b49824632b80e16a070316f7bf7a88
Michael Gerfen <gerfen@hotmail.com>
2023-07-13 12:47:22 -0700

    WIP:  Creating a lighter weight token display ocntrol to allow better UI rendering performance for Aalignment bulk edit.

---
+++ src/ClearDashboard.Wpf.Application/UserControls/ReadOnlyTokenDisplay.xaml.cs
@@ -0,0 +1,16 @@
+﻿using ClearBible.Engine.Corpora;
+
+namespace ClearDashboard.Wpf.Application.UserControls
+{
+    /// <summary>
+    /// A control for displaying a single <see cref="Token"/> with no interactivity.
+    /// </summary>
+    public partial class ReadOnlyTokenDisplay
+    {
+        public ReadOnlyTokenDisplay()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
