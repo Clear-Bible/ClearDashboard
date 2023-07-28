63d9be9dc82529b9ed82a23e3d133775974ede71
Robertson Brinker <robertsonbrinker@gmail.com>
2023-07-28 16:51:22 -0400

    added reference

--- tools/GenerateLicenseKeyForDashboard/Views/ShellView.xaml.cs
+++ tools/GenerateLicenseKeyForDashboard/Views/ShellView.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
+using GenerateLicenseKeyForDashboard.Models;
 using GenerateLicenseKeyForDashboard.ViewModels;
 
 namespace GenerateLicenseKeyForDashboard.Views
