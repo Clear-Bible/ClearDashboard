a473891d2ae9a1193d8e6ba3b7e1783ec115d30f
GeoDirk <geodirk@gmail.com>
2023-09-01 14:17:13 -0400

    localization

--- src/ClearDashboard.Wpf.Application/Models/JiraTicketResponse.cs
+++ src/ClearDashboard.Wpf.Application/Models/JiraTicketResponse.cs
@@ -5,6 +5,6 @@ namespace ClearDashboard.Wpf.Application.Models;
 public class JiraTicketResponse
 {
     public int Id { get; set; }
-    public string Key { get; set; }
+    public string Key { get; set; } = string.Empty;
     public Uri Self { get; set; }
 }
