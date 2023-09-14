b9cdf065af3efbf16b62f7149c33c45a96163bf6
Michael Gerfen <gerfen@hotmail.com>
2023-09-14 13:45:47 -0700

    WIP:  Lexicon import

---
+++ src/ClearDashboard.Wpf.Application/ViewModels/Lexicon/LexiconViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Caliburn.Micro;
+using ClearDashboard.Wpf.Application.Services;
+using ClearDashboard.Wpf.Application.ViewModels.Panes;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ClearDashboard.Wpf.Application.ViewModels.Lexicon
+{
+    public class LexiconViewModel : ToolViewModel
+    {
+        public LexiconViewModel()
+        {
+            
+        }
+        public LexiconViewModel(INavigationService navigationService, 
+            ILogger<LexiconViewModel> logger, 
+            DashboardProjectManager dashboardProjectManager, 
+            IEventAggregator eventAggregator, 
+            IMediator mediator, 
+            ILifetimeScope lifetimeScope, 
+            ILocalizationService localizationService) :
+            base(navigationService, logger, dashboardProjectManager, eventAggregator, mediator, lifetimeScope, localizationService)
+        {
+            
+        }
+    }
+}
