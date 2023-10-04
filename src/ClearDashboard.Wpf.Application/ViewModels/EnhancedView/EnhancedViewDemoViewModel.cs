c5f1c5f4d4061f102a1a87011c3700038d984064
GeoDirk <geodirk@gmail.com>
2023-10-04 12:13:51 -0400

    working search...finally

--- src/ClearDashboard.Wpf.Application/ViewModels/EnhancedView/EnhancedViewDemoViewModel.cs
+++ src/ClearDashboard.Wpf.Application/ViewModels/EnhancedView/EnhancedViewDemoViewModel.cs
@@ -276,6 +276,11 @@ namespace ClearDashboard.Wpf.Application.ViewModels.EnhancedView
             NotePaneVisibility = Visibility.Visible;
         }
 
+        public void FilterPinsTarget(NoteEventArgs e)
+        {
+            NotePaneVisibility = Visibility.Visible;
+        }
+
         public void FilterPinsByBiblicalTerms(NoteEventArgs e)
         {
             NotePaneVisibility = Visibility.Visible;
