b759eb1b78f62f328cb9fd600c2754219ac5fdcf
RM <russell.morley@clear.bible>
2023-11-02 15:50:28 -0700

    Added external note add comment and resolve

--- src/ClearDashboard.ParatextPlugin.CQRS/Features/Notes/GetNotesQuery.cs
+++ src/ClearDashboard.ParatextPlugin.CQRS/Features/Notes/GetNotesQuery.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using ClearDashboard.DAL.CQRS;
-using ClearDashboard.DataAccessLayer.Models;
 using MediatR;
 
 namespace ClearDashboard.ParatextPlugin.CQRS.Features.Notes
