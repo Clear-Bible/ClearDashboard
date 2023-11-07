1dd124c04af92e7f0fac710ee6ddefeeadda649e
GeoDirk <geodirk@gmail.com>
2023-11-07 12:29:17 -0500

    Works but errors in ClearEngine on some books

--- src/ClearDashboard.Wpf.Application/ViewModels/Project/ProjectDesignSurfaceViewModel.cs
+++ src/ClearDashboard.Wpf.Application/ViewModels/Project/ProjectDesignSurfaceViewModel.cs
@@ -915,15 +915,25 @@ namespace ClearDashboard.Wpf.Application.ViewModels.Project
                                     .Tokenize<LatinWordTokenizer>()
                                     .Transform<IntoTokensTextRowProcessor>()
                                     .Transform<SetTrainingBySurfaceLowercase>(),
+
                                 Tokenizers.WhitespaceTokenizer =>
                                     (await ParatextProjectTextCorpus.Get(Mediator!, selectedProject.Id!, bookIds, cancellationToken))
                                     .Tokenize<WhitespaceTokenizer>()
                                     .Transform<IntoTokensTextRowProcessor>()
                                     .Transform<SetTrainingBySurfaceLowercase>(),
-                                Tokenizers.ZwspWordTokenizer => (await ParatextProjectTextCorpus.Get(Mediator!, selectedProject.Id!, bookIds, cancellationToken))
+
+                                Tokenizers.ZwspWordTokenizer => 
+                                    (await ParatextProjectTextCorpus.Get(Mediator!, selectedProject.Id!, bookIds, cancellationToken))
                                     .Tokenize<ZwspWordTokenizer>()
                                     .Transform<IntoTokensTextRowProcessor>()
                                     .Transform<SetTrainingBySurfaceLowercase>(),
+
+                                Tokenizers.ChineseBibleWordTokenizer => 
+                                    (await ParatextProjectTextCorpus.Get(Mediator!, selectedProject.Id!, bookIds, cancellationToken))
+                                    .Tokenize<ChineseBibleWordTokenizer>()
+                                    .Transform<IntoTokensTextRowProcessor>()
+                                    .Transform<SetTrainingBySurfaceLowercase>(),
+
                                 _ => (await ParatextProjectTextCorpus.Get(Mediator!, selectedProject.Id!, null, cancellationToken))
                                     .Tokenize<WhitespaceTokenizer>()
                                     .Transform<IntoTokensTextRowProcessor>()
