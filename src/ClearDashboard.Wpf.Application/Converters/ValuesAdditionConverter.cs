e9a052ca0a98b3f808551e9dc05a62ac872656d1
GeoDirk <geodirk@gmail.com>
2023-11-07 21:38:06 -0500

    ui wip

---
+++ src/ClearDashboard.Wpf.Application/Converters/ValuesAdditionConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace ClearDashboard.Wpf.Application.Converters
+{
+    public class ValuesAdditionConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// Adds all the values passed as parameters and returns the sum.
+        /// </summary>
+        /// <param name="values"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values == null || values.Count() < 2)
+            {
+                return 0;
+            }
+
+            double sum = 0;
+            for (int i = 0; i < values.Count(); i++)
+            {
+                sum += (double)values[i];
+            }
+
+            return sum;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
