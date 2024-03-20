416c62e589ef548c4fcd43071e7167ab968f34e6
GeoDirk <geodirk@gmail.com>
2024-03-20 13:13:50 -0400

    bandaid fix for this problem

--- src/ClearDashboard.DAL/Features/DashboardProjects/UpdateLabelGroupHyphenSlice.cs
+++ src/ClearDashboard.DAL/Features/DashboardProjects/UpdateLabelGroupHyphenSlice.cs
@@ -9,46 +9,54 @@ using System.Threading.Tasks;
 
 namespace ClearDashboard.DataAccessLayer.Features.DashboardProjects
 {
+    public record UpdateDatabaseHyphenQuery(DashboardProject Project) : IRequest<RequestResult<string>>;
 
-        public record UpdateLabelGroupHyphenQuery(DashboardProject Project) : IRequest<RequestResult<string>>;
-
-        public class UpdateLabelGroupHyphenQueryHandler : SqliteDatabaseRequestHandler<UpdateLabelGroupHyphenQuery, RequestResult<string>, string>
+    public class UpdateDatabaseHyphenQueryHandler : SqliteDatabaseRequestHandler<UpdateDatabaseHyphenQuery, RequestResult<string>, string>
+    {
+        public UpdateDatabaseHyphenQueryHandler(ILogger<UpdateDatabaseHyphenQueryHandler> logger) : base(logger)
         {
-            public UpdateLabelGroupHyphenQueryHandler(ILogger<UpdateLabelGroupHyphenQueryHandler> logger) : base(logger)
-            {
-                //no-op
-            }
-
-            protected override string ResourceName { get; set; }
-
-            public override Task<RequestResult<string>> Handle(UpdateLabelGroupHyphenQuery request, CancellationToken cancellationToken)
-            {
-                FileInfo fi = new FileInfo(request.Project.FullFilePath);
-
-                ResourceDirectory = fi.DirectoryName;
-                ResourceName = fi.Name;
-
-                var queryResult = ValidateResourcePath(string.Empty);
-                if (queryResult.Success)
-                {
-                    try
-                    {
-                        queryResult.Data = ExecuteSqliteCommandAndProcessData($"UPDATE LABELGROUP SET NAME=REPLACE(NAME,'_','-')");
-                    }
-                    catch
-                    {
-                        queryResult.Success = false;
-                    }
-                }
-                return Task.FromResult(queryResult);
-            }
-
-            protected override string ProcessData()
-            {
-                return "";
-            }
+            //no-op
         }
 
-    
- 
+        protected override string ResourceName { get; set; }
+
+        public override Task<RequestResult<string>> Handle(UpdateDatabaseHyphenQuery request, CancellationToken cancellationToken)
+        {
+            var fi = new FileInfo(request.Project.FullFilePath);
+
+            ResourceDirectory = fi.DirectoryName!;
+            ResourceName = fi.Name;
+
+            var queryResult = ValidateResourcePath(string.Empty);
+            if (queryResult.Success)
+            {
+                try
+                {
+                    // fix the hyphens in the LABELGROUP table
+                    queryResult.Data = ExecuteSqliteCommandAndProcessData($"UPDATE LABELGROUP SET NAME=REPLACE(NAME,'_','-')");
+                }
+                catch
+                {
+                    queryResult.Success = false;
+                }
+
+                try
+                {
+                    // fix the hyphens in the LABEL table
+                    queryResult.Data = ExecuteSqliteCommandAndProcessData($"UPDATE LABEL SET TEXT=REPLACE(TEXT,'_','-')");
+                }
+                catch
+                {
+                    queryResult.Success = false;
+                }
+            }
+            return Task.FromResult(queryResult);
+        }
+
+        protected override string ProcessData()
+        {
+            return "";
+        }
+    }
+
 }
