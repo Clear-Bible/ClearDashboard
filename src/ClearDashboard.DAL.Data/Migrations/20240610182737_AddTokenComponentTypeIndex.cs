4ae1df47be1d075987bb238a53e8c191fb902a23
Michael Gerfen <gerfen@hotmail.com>
2024-06-10 12:18:31 -0700

    Added initial population of grammar types.

---
+++ src/ClearDashboard.DAL.Data/Migrations/20240610182737_AddTokenComponentTypeIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClearDashboard.DataAccessLayer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTokenComponentTypeIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_TokenComponent_Type",
+                table: "TokenComponent",
+                column: "Type");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TokenComponent_Type",
+                table: "TokenComponent");
+        }
+    }
+}
