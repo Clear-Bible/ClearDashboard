8e0979c2b223d4817b3722ca55c232924a491dd0
GeoDirk <geodirk@gmail.com>
2024-06-26 15:38:50 -0400

    modified the namespace

--- src/ClearDashboard.DAL.WpfViewModels/PinsVerseList.cs
+++ src/ClearDashboard.DAL.WpfViewModels/PinsVerseList.cs
@@ -1,4 +1,6 @@
-﻿namespace ClearDashboard.DAL.WpfViewModels
+﻿using ClearDashboard.DAL.Wpf.ViewModels;
+
+namespace ClearDashboard.DAL.Wpf.ViewModels
 {
     public class PinsVerseListViewModel: VerseViewModel
     {
