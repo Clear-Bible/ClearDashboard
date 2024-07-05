9359ebbf2c9c9076b437ab04cef05fc0e4bd3f6a
Michael Gerfen <gerfen@hotmail.com>
2024-07-05 09:44:10 -0700

    Removed magic string.

--- src/ClearDashboard.Wpf.Application.Abstractions/Services/VerseManager.cs
+++ src/ClearDashboard.Wpf.Application.Abstractions/Services/VerseManager.cs
@@ -11,6 +11,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
+using ClearDashboard.DataAccessLayer.Models;
+using Token = ClearBible.Engine.Corpora.Token;
 
 namespace ClearDashboard.Wpf.Application.Services
 {
@@ -38,7 +40,7 @@ namespace ClearDashboard.Wpf.Application.Services
                 // IsParallelCorpusToken so we can tell if this is CompositeToken(parallel) or CompositeToken(null)
                 if (parallelCorpusId != null)
                 {
-                    compositeToken.Metadata["IsParallelCorpusToken"] = true;
+                    compositeToken.Metadata[MetadatumKeys.IsParallelCorpusToken] = true;
                 }
            
                 await TokenizedTextCorpus.PutCompositeToken(Mediator, compositeToken, parallelCorpusId);
